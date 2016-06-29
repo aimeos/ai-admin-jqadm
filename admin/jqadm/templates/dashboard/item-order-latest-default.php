7c1186e07c08c6f116c7a1b655f6fe62c93e4f85
Aimeos <aimeos@aimeos.org>
2016-06-29 14:35:47 +0200

    Added panel for payment type diagram

--- admin/jqadm/templates/dashboard/item-order-latest-default.php
+++ admin/jqadm/templates/dashboard/item-order-latest-default.php
@@ -2,7 +2,7 @@
 
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Aimeos (aimeos.org), 2015
+ * @copyright Aimeos (aimeos.org), 2016
  */
 
 $price = function( array $orders, \Aimeos\MShop\Order\Item\Iface $item, $priceFormat )
---
+++ admin/jqadm/templates/dashboard/item-order-paymenttype-default.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2016
+ */
+
+$enc = $this->encoder();
+
+?>
+
+<div class="order-paymenttype card panel col-lg-6">
+	<div id="order-paymenttype-head" class="header card-header">
+		<?php echo $enc->html( $this->translate( 'admin', 'Payment types' ) ); ?>
+	</div>
+	<div id="order-paymenttype-data" class="content card-block">
+	</div>
+</div>
+<?php echo $this->get( 'orderpaymenttypeBody' ); ?>
