f18a2c59fd29661ebd8fef0dfe566ef5115d8452
Aimeos <aimeos@aimeos.org>
2016-06-30 19:08:11 +0200

    Added panel for orders per day

---
+++ admin/jqadm/templates/dashboard/item-order-day-default.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2016
+ */
+
+$enc = $this->encoder();
+
+?>
+
+<div class="order-day card panel col-lg-12">
+	<div id="order-day-head" class="header card-header">
+		<?php echo $enc->html( $this->translate( 'admin', 'Orders by day' ) ); ?>
+	</div>
+	<div id="order-day-data" class="content card-block">
+	</div>
+</div>
+<?php echo $this->get( 'orderdayBody' ); ?>
---
+++ admin/jqadm/tests/Admin/JQAdm/Dashboard/Order/Day/StandardTest.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2016
+ */
+
+
+namespace Aimeos\Admin\JQAdm\Dashboard\Order\Day;
+
+
+class StandardTest extends \PHPUnit_Framework_TestCase
+{
+	private $context;
+	private $object;
+	private $view;
+
+
+	protected function setUp()
+	{
+		$this->view = \TestHelperJqadm::getView();
+		$this->context = \TestHelperJqadm::getContext();
+		$templatePaths = \TestHelperJqadm::getTemplatePaths();
+
+		$this->object = new \Aimeos\Admin\JQAdm\Dashboard\Order\Day\Standard( $this->context, $templatePaths );
+		$this->object->setView( $this->view );
+	}
+
+
+	protected function tearDown()
+	{
+		unset( $this->object );
+	}
+
+
+	public function testCreate()
+	{
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$this->object->create();
+	}
+
+
+	public function testCopy()
+	{
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$this->object->copy();
+	}
+
+
+	public function testDelete()
+	{
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$this->object->delete();
+	}
+
+
+	public function testGet()
+	{
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$this->object->get();
+	}
+
+
+	public function testSave()
+	{
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$this->object->save();
+	}
+
+
+	public function testSearch()
+	{
+		$result = $this->object->search();
+
+		$this->assertContains( '<div class="order-day', $result );
+	}
+
+
+	public function testSearchException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Dashboard\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'getSubClients' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \Exception() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->search();
+	}
+
+
+	public function testSearchMShopException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Dashboard\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'getSubClients' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \Aimeos\MShop\Exception() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->search();
+	}
+
+
+	public function testGetSubClient()
+	{
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$this->object->getSubClient( 'invalid' );
+	}
+
+
+	protected function getViewNoRender()
+	{
+		return $this->getMockBuilder( '\Aimeos\MW\View\Standard' )
+			->setConstructorArgs( array( array() ) )
+			->setMethods( array( 'render', 'config' ) )
+			->getMock();
+	}
+}
