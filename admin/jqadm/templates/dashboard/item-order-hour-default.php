b44565f03490a8f2e91537846421980abcc1b08d
Aimeos <aimeos@aimeos.org>
2016-06-30 09:21:28 +0200

    Added panel for orders by hours

---
+++ admin/jqadm/templates/dashboard/item-order-hour-default.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2016
+ */
+
+$enc = $this->encoder();
+
+?>
+
+<div class="order-hour card panel col-lg-6">
+	<div id="order-hour-head" class="header card-header">
+		<?php echo $enc->html( $this->translate( 'admin', 'Orders by hour' ) ); ?>
+	</div>
+	<div id="order-hour-data" class="content card-block">
+	</div>
+</div>
+<?php echo $this->get( 'orderhourBody' ); ?>
