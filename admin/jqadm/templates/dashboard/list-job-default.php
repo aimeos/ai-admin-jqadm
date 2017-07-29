2f319e9e91db8a0a39f11d4a26268e7cfdb174a5
Aimeos <aimeos@aimeos.org>
2017-07-29 14:31:15 +0200

    Implement download job panel in dashboard

---
+++ admin/jqadm/templates/dashboard/list-job-default.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2017
+ */
+
+$enc = $this->encoder();
+$items = $this->get( 'jobItems', [] );
+
+$getTarget = $this->config( 'admin/jqadm/url/get/target' );
+$getCntl = $this->config( 'admin/jqadm/url/get/controller', 'Jqadm' );
+$getAction = $this->config( 'admin/jqadm/url/get/action', 'get' );
+$getConfig = $this->config( 'admin/jqadm/url/get/config', [] );
+
+$delTarget = $this->config( 'admin/jqadm/url/delete/target' );
+$delCntl = $this->config( 'admin/jqadm/url/delete/controller', 'Jqadm' );
+$delAction = $this->config( 'admin/jqadm/url/delete/action', 'delete' );
+$delConfig = $this->config( 'admin/jqadm/url/delete/config', [] );
+
+
+?>
+<?php if( !empty( $items ) ) : ?>
+	<div class="dashboard-job row">
+		<div class="job-list card col-lg-12">
+			<div id="job-list-head" class="card-header header" role="tab"
+				data-toggle="collapse" data-target="#job-list-data"
+				aria-expanded="true" aria-controls="job-list-data">
+				<div class="card-tools-left">
+					<div class="btn btn-card-header act-show fa"></div>
+				</div>
+				<span class="item-label header-label">
+					<?= $enc->html( $this->translate( 'admin', 'Import/Export jobs' ) ); ?>
+				</span>
+			</div>
+			<div id="job-list-data" class="card-block content collapse show" role="tabpanel" aria-labelledby="job-list-head">
+				<div class="table-responsive">
+					<table class="list-items table table-hover">
+						<tbody>
+							<?php foreach( $items as $id => $item ) : ?>
+								<tr>
+									<td class="job-label"><?= $enc->html( $item->getLabel() ); ?></td>
+									<td class="job-mtime"><?= $enc->html( $item->getTimeModified() ); ?></td>
+									<td class="actions">
+										<?php if( !$this->site()->readonly( $item->getSiteId() ) ) : ?>
+											<a class="btn act-delete fa" tabindex="1"
+												href="<?= $enc->attr( $this->url( $delTarget, $delCntl, $delAction, ['resource' => 'job', 'id' => $id], [], $delConfig ) ); ?>"
+												title="<?= $enc->attr( $this->translate( 'admin', 'Delete this entry') ); ?>"
+												aria-label="<?= $enc->attr( $this->translate( 'admin', 'Delete' ) ); ?>"></a>
+										<?php endif; ?>
+										<a class="btn act-download fa" tabindex="1"
+											href="<?= $enc->attr( $this->url( $getTarget, $getCntl, $getAction, ['resource' => 'job', 'id' => $id], [], $getConfig ) ); ?>"
+											title="<?= $enc->attr( $this->translate( 'admin', 'Download this entry') ); ?>"
+											aria-label="<?= $enc->attr( $this->translate( 'admin', 'Download' ) ); ?>"></a>
+									</td>
+								</tr>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php endif; ?>
---
+++ admin/jqadm/tests/Admin/JQAdm/Dashboard/Job/StandardTest.php
@@ -0,0 +1,199 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2017
+ */
+
+
+namespace Aimeos\Admin\JQAdm\Dashboard\Job;
+
+
+class StandardTest extends \PHPUnit\Framework\TestCase
+{
+	private $context;
+	private $object;
+	private $view;
+
+
+	protected function setUp()
+	{
+		$this->view = \TestHelperJqadm::getView();
+		$this->context = \TestHelperJqadm::getContext();
+		$templatePaths = \TestHelperJqadm::getTemplatePaths();
+
+		$this->object = new \Aimeos\Admin\JQAdm\Dashboard\Job\Standard( $this->context, $templatePaths );
+		$this->object->setView( $this->view );
+	}
+
+
+	protected function tearDown()
+	{
+		unset( $this->object );
+	}
+
+
+	public function testDelete()
+	{
+		$manager = \Aimeos\MAdmin\Factory::createManager( $this->context, 'job' );
+		$item = $manager->createItem();
+		$item->setLabel( 'jobdeletetest.csv' );
+		$item->setResult( ['file' => 'jobdeletetest.csv'] );
+		$manager->saveItem( $item );
+
+		$param = ['site' => 'unittest', 'id' => $item->getId()];
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $param );
+		$this->view->addHelper( 'param', $helper );
+
+		$result = $this->object->delete();
+
+		$this->assertContains( 'dashboard-job', $result );
+		$this->assertNotContains( 'jobdeletetest.csv', $result );
+	}
+
+
+	public function testDeleteException()
+	{
+		$mock = $this->getMockBuilder( '\Aimeos\MW\Filesystem\Manager\Standard' )
+			->disableOriginalConstructor()
+			->setMethods( ['get'] )
+			->getMock();
+
+		$mock->expects( $this->once() )->method( 'get' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$this->context->setFileSystemManager( $mock );
+		$this->object->setView( $this->getViewNoRender() );
+		$this->object->delete();
+	}
+
+
+	public function testDeleteMAdminException()
+	{
+		$mock = $this->getMockBuilder( '\Aimeos\MW\Filesystem\Manager\Standard' )
+			->disableOriginalConstructor()
+			->setMethods( ['get'] )
+			->getMock();
+
+		$mock->expects( $this->once() )->method( 'get' )
+			->will( $this->throwException( new \Aimeos\MAdmin\Exception() ) );
+
+		$this->context->setFileSystemManager( $mock );
+		$this->object->setView( $this->getViewNoRender() );
+		$this->object->delete();
+	}
+
+
+	public function testGet()
+	{
+		$path = dirname( dirname( dirname( dirname( __DIR__ ) ) ) ) . '/tmp/jobtest.csv';
+
+		if( touch( $path ) === false ) {
+			throw new \Exception( 'Cannot create test file' );
+		}
+
+		$manager = \Aimeos\MAdmin\Factory::createManager( $this->context, 'job' );
+		$item = $manager->createItem();
+		$item->setLabel( 'jobtest.csv' );
+		$item->setResult( ['file' => 'jobtest.csv'] );
+		$manager->saveItem( $item );
+
+		$result = $this->object->get();
+
+		$this->assertNull( $result );
+
+		$manager->deleteItem( $item->getId() );
+		unlink( $path );
+	}
+
+
+	public function testGetException()
+	{
+		$mock = $this->getMockBuilder( '\Aimeos\MW\Filesystem\Manager\Standard' )
+			->disableOriginalConstructor()
+			->setMethods( ['get'] )
+			->getMock();
+
+		$mock->expects( $this->once() )->method( 'get' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$this->context->setFileSystemManager( $mock );
+		$this->object->setView( $this->getViewNoRender() );
+		$this->object->get();
+	}
+
+
+	public function testGetMAdminException()
+	{
+		$mock = $this->getMockBuilder( '\Aimeos\MW\Filesystem\Manager\Standard' )
+			->disableOriginalConstructor()
+			->setMethods( ['get'] )
+			->getMock();
+
+		$mock->expects( $this->once() )->method( 'get' )
+			->will( $this->throwException( new \Aimeos\MAdmin\Exception() ) );
+
+		$this->context->setFileSystemManager( $mock );
+		$this->object->setView( $this->getViewNoRender() );
+		$this->object->get();
+	}
+
+
+	public function testSearch()
+	{
+		$result = $this->object->search();
+
+		$this->assertContains( 'dashboard-job', $result );
+		$this->assertContains( 'unittest job', $result );
+	}
+
+
+	public function testSearchException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Dashboard\Job\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'getSubClients' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->search();
+	}
+
+
+	public function testSearchMAdminException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Dashboard\Job\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'getSubClients' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \Aimeos\MAdmin\Exception() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->search();
+	}
+
+
+	protected function getViewNoRender()
+	{
+		$view = $this->getMockBuilder( '\Aimeos\MW\View\Standard' )
+			->setConstructorArgs( array( [] ) )
+			->setMethods( array( 'render', 'config' ) )
+			->getMock();
+
+		$param = ['site' => 'unittest', 'id' => -1];
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $view, $param );
+		$view->addHelper( 'param', $helper );
+
+		$helper = new \Aimeos\MW\View\Helper\Access\Standard( $view, [] );
+		$view->addHelper( 'access', $helper );
+
+		return $view;
+	}
+}
