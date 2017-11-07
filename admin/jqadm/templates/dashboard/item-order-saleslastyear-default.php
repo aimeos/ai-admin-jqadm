6479318c31e9da2df81155360b0f38d68c884c9a
DennisDittmann <dennis.dittmann@km2.de>
2017-11-07 10:11:46 +0100

    Create item-order-saleslastyear-default.php
    Template for saleslastyear subpart

---
+++ admin/jqadm/templates/dashboard/item-order-saleslastyear-default.php
@@ -0,0 +1,14 @@
+<div class="order-saleslastyear card col-lg-12">
+	<div id="order-saleslastyear-head" class="card-header header" role="tab"
+		 data-toggle="collapse" data-target="#order-saleslastyear-data"
+		 aria-expanded="true" aria-controls="order-saleslastyear-data">
+		<div class="card-tools-left">
+			<div class="btn btn-card-header act-show fa"></div>
+		</div>
+		<span class="item-label header-label">
+			Gesamtumsatz der letzten 12 Monate
+		</span>
+	</div>
+	<div id="order-saleslastyear-data" class="card-block collapse show content loading" role="tabpanel" aria-labelledby="order-saleslastyear-head">
+	</div>
+</div>
