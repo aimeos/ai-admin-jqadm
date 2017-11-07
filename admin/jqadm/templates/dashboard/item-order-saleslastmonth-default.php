87ccd4cb4709b9414b6b06bb4f20cd76505c4622
DennisDittmann <dennis.dittmann@km2.de>
2017-11-07 10:11:00 +0100

    Create item-order-saleslastmonth-default.php
    Template for saleslastmonth subpart

---
+++ admin/jqadm/templates/dashboard/item-order-saleslastmonth-default.php
@@ -0,0 +1,14 @@
+<div class="order-saleslastmonth card col-lg-12">
+	<div id="order-saleslastmonth-head" class="card-header header" role="tab"
+		 data-toggle="collapse" data-target="#order-saleslastmonth-data"
+		 aria-expanded="true" aria-controls="order-saleslastmonth-data">
+		<div class="card-tools-left">
+			<div class="btn btn-card-header act-show fa"></div>
+		</div>
+		<span class="item-label header-label">
+			Umsatzentwicklung der letzten 30 Tage
+		</span>
+	</div>
+	<div id="order-saleslastmonth-data" class="card-block collapse show content loading" role="tabpanel" aria-labelledby="order-saleslastmonth-head">
+	</div>
+</div>
