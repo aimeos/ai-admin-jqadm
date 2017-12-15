0c0a9d1d210d7755f32cf577af0e564b04ef70bd
Aimeos <aimeos@aimeos.org>
2017-12-15 13:03:06 +0100

    Parse error bugfix

--- admin/jqadm/templates/common/partials/columns-default.php
+++ admin/jqadm/templates/common/partials/columns-default.php
@@ -28,12 +28,12 @@ $names = array_merge( (array) $this->get( 'group', [] ), ['fields', ''] );
 
 ?>
 <div class="dropdown filter-columns">
-	<button class="btn act-columns fa" type="button" id="dropdownMenuButton-<?= $this->( 'group' ) ?>"
+	<button class="btn act-columns fa" type="button" id="dropdownMenuButton-<?= $this->get( 'group' ) ?>"
 		data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" tabindex="<?= $this->get( 'tabindex', 1 ); ?>"
 		aria-label="<?= $enc->attr( $this->translate( 'admin', 'Columns' ) ); ?>"
 		title="<?= $enc->attr( $this->translate( 'admin', 'Columns') ); ?>">
 	</button>
-	<ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton-<?= $this->( 'group' ) ?>">
+	<ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton-<?= $this->get( 'group' ) ?>">
 		<?php foreach( $this->get( 'data', [] ) as $key => $name ) : ?>
 			<li class="dropdown-item">
 				<a href="#"><label>
