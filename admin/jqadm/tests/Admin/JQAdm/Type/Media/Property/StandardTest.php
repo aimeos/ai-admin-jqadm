58e2f3f8e7a3f76601d878cd978093d6fd49ced2
Aimeos <aimeos@aimeos.org>
2018-01-03 15:31:40 +0100

    Implemented media property type panel

---
+++ admin/jqadm/tests/Admin/JQAdm/Type/Media/Property/StandardTest.php
@@ -0,0 +1,371 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2018
+ */
+
+
+namespace Aimeos\Admin\JQAdm\Type\Media\Property;
+
+
+class StandardTest extends \PHPUnit\Framework\TestCase
+{
+	private $context;
+	private $object;
+	private $view;
+
+
+	protected function setUp()
+	{
+		$this->view = \TestHelperJqadm::getView();
+		$this->context = \TestHelperJqadm::getContext();
+
+		$this->object = new \Aimeos\Admin\JQAdm\Type\Media\Property\Standard( $this->context );
+		$this->object = new \Aimeos\Admin\JQAdm\Common\Decorator\Page( $this->object, $this->context );
+		$this->object->setAimeos( \TestHelperJqadm::getAimeos() );
+		$this->object->setView( $this->view );
+	}
+
+
+	protected function tearDown()
+	{
+		unset( $this->object, $this->view, $this->context );
+	}
+
+
+	public function testCreate()
+	{
+		$this->object->create();
+	}
+
+
+	public function testCreateException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Type\Media\Property\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'getSubClients' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->create();
+	}
+
+
+	public function testCreateMShopException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Type\Media\Property\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'getSubClients' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \Aimeos\MShop\Exception() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->create();
+	}
+
+
+	public function testCopy()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'media/property/type' );
+
+		$param = ['type' => 'unittest', 'id' => $manager->findItem( 'size', [], 'media' )->getId()];
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $param );
+		$this->view->addHelper( 'param', $helper );
+
+		$result = $this->object->copy();
+
+		$this->assertContains( 'size', $result );
+	}
+
+
+	public function testCopyException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Type\Media\Property\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'getSubClients' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->copy();
+	}
+
+
+	public function testCopyMShopException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Type\Media\Property\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'getSubClients' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \Aimeos\MShop\Exception() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->copy();
+	}
+
+
+	public function testDelete()
+	{
+		$this->assertNotNull( $this->object->delete() );
+	}
+
+
+	public function testDeleteException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Type\Media\Property\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'getSubClients', 'search' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->delete();
+	}
+
+
+	public function testDeleteMShopException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Type\Media\Property\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'getSubClients', 'search' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \Aimeos\MShop\Exception() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->delete();
+	}
+
+
+	public function testGet()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'media/property/type' );
+
+		$param = ['type' => 'unittest', 'id' => $manager->findItem( 'size', [], 'media' )->getId()];
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $param );
+		$this->view->addHelper( 'param', $helper );
+
+		$result = $this->object->get();
+
+		$this->assertContains( 'size', $result );
+	}
+
+
+	public function testGetException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Type\Media\Property\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'getSubClients' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->get();
+	}
+
+
+	public function testGetMShopException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Type\Media\Property\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'getSubClients' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \Aimeos\MShop\Exception() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->get();
+	}
+
+
+	public function testGetViewException()
+	{
+		$object = new \Aimeos\Admin\JQAdm\Type\Media\Property\Standard( $this->context, [] );
+
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$object->getView();
+	}
+
+
+	public function testSave()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'media/property/type' );
+
+		$param = array(
+			'type' => 'unittest',
+			'item' => array(
+				'media.property.type.id' => '',
+				'media.property.type.status' => '1',
+				'media.property.type.domain' => 'product',
+				'media.property.type.code' => 'jqadm@test',
+				'media.property.type.label' => 'jqadm test',
+			),
+		);
+
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $param );
+		$this->view->addHelper( 'param', $helper );
+
+		$this->object->save();
+
+		$manager->deleteItem( $manager->findItem( 'jqadm@test', [], 'product' )->getId() );
+	}
+
+
+	public function testSaveException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Type\Media\Property\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'fromArray' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'fromArray' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->save();
+	}
+
+
+	public function testSaveMShopException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Type\Media\Property\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'fromArray' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'fromArray' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->save();
+	}
+
+
+	public function testSaveJQAdmException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Type\Media\Property\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'fromArray' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'fromArray' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->save();
+	}
+
+
+	public function testSearch()
+	{
+		$param = array(
+			'type' => 'unittest', 'lang' => 'de',
+			'filter' => array(
+				'key' => array( 0 => 'media.property.type.code' ),
+				'op' => array( 0 => '==' ),
+				'val' => array( 0 => 'size' ),
+			),
+			'sort' => array( '-media.property.type.id' ),
+		);
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $param );
+		$this->view->addHelper( 'param', $helper );
+
+		$result = $this->object->search();
+
+		$this->assertContains( '>size<', $result );
+	}
+
+
+	public function testSearchException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Type\Media\Property\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'initCriteria' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'initCriteria' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->search();
+	}
+
+
+	public function testSearchMShopException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Type\Media\Property\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'initCriteria' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'initCriteria' )
+			->will( $this->throwException( new \Aimeos\MShop\Exception() ) );
+
+		$object->setView( $this->getViewNoRender() );
+
+		$object->search();
+	}
+
+
+	public function testGetSubClientInvalid()
+	{
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$this->object->getSubClient( '$unknown$' );
+	}
+
+
+	public function testGetSubClientUnknown()
+	{
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$this->object->getSubClient( 'unknown' );
+	}
+
+
+	protected function getViewNoRender()
+	{
+		$view = $this->getMockBuilder( '\Aimeos\MW\View\Standard' )
+			->setConstructorArgs( array( [] ) )
+			->setMethods( array( 'render', 'config' ) )
+			->getMock();
+
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'media/property/type' );
+
+		$param = ['type' => 'unittest', 'id' => $manager->findItem( 'size', [], 'media' )->getId()];
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $view, $param );
+		$view->addHelper( 'param', $helper );
+
+		$helper = new \Aimeos\MW\View\Helper\Access\Standard( $view, [] );
+		$view->addHelper( 'access', $helper );
+
+		return $view;
+	}
+
+}
