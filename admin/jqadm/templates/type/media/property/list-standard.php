84dd435fd80754f3065f4df10aab3f419272fa76
Aimeos <aimeos@aimeos.org>
2018-01-03 21:20:09 +0100

    Translation update

--- admin/jqadm/templates/type/media/property/list-standard.php
+++ admin/jqadm/templates/type/media/property/list-standard.php
@@ -81,7 +81,7 @@ $columnList = [
 <nav class="main-navbar">
 
 	<span class="navbar-brand">
-		<?= $enc->html( $this->translate( 'admin', 'Media Lists Types' ) ); ?>
+		<?= $enc->html( $this->translate( 'admin', 'Media Property Types' ) ); ?>
 		<span class="navbar-secondary">(<?= $enc->html( $this->site()->label() ); ?>)</span>
 	</span>
 
