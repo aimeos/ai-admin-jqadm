8be135eb78df098eb1fa4b96a169fc5c4da78279
Aimeos <aimeos@aimeos.org>
2018-01-04 13:58:24 +0100

    Implements image sub-panel for supplier items

---
+++ admin/jqadm/templates/supplier/item-image-standard.php
@@ -0,0 +1,295 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2018
+ */
+
+
+$enc = $this->encoder();
+
+$keys = [
+	'supplier.lists.id', 'supplier.lists.siteid', 'supplier.lists.typeid', 'supplier.lists.datestart', 'supplier.lists.dateend',
+	'media.siteid', 'media.preview', 'media.label', 'media.status', 'media.typeid', 'media.languageid'
+];
+
+
+?>
+<div id="image" class="item-image content-block tab-pane fade" role="tablist" aria-labelledby="image">
+	<div id="item-image-group" role="tablist" aria-multiselectable="true"
+		data-items="<?= $enc->attr( json_encode( $this->get( 'imageData', [] ) ) ); ?>"
+		data-listtypeid="<?= key( $this->get( 'imageListTypes', [] ) ) ?>"
+		data-keys="<?= $enc->attr( json_encode( $keys ) ) ?>"
+		data-siteid="<?= $this->site()->siteid() ?>" >
+
+		<div v-for="(id, idx) in items['supplier.lists.id']" class="group-item card">
+			<input class="item-listid" type="hidden" name="<?= $enc->attr( $this->formparam( array( 'image', 'supplier.lists.id', '' ) ) ); ?>"
+				v-bind:value="items['supplier.lists.id'][idx]" />
+
+			<div v-bind:id="'item-image-group-item-' + idx" v-bind:class="getCss(idx)"
+				v-bind:data-target="'#item-image-group-data-' + idx" data-toggle="collapse" role="tab" class="card-header header"
+				v-bind:aria-controls="'item-image-group-data-' + idx" aria-expanded="false">
+				<div class="card-tools-left">
+					<div class="btn btn-card-header act-show fa" tabindex="<?= $this->get( 'tabindex' ); ?>"
+						title="<?= $enc->attr( $this->translate( 'admin', 'Show/hide this entry') ); ?>">
+					</div>
+				</div>
+				<span class="item-label header-label">{{ getLabel(idx) }}</span>
+				&nbsp;
+				<div class="card-tools-right">
+					<div v-if="!checkSite('supplier.lists.siteid', idx)"
+						class="btn btn-card-header act-delete fa" tabindex="<?= $this->get( 'tabindex' ); ?>"
+						title="<?= $enc->attr( $this->translate( 'admin', 'Delete this entry') ); ?>"
+						v-on:click.stop="removeItem(idx)">
+					</div>
+				</div>
+			</div>
+
+			<div v-bind:id="'item-image-group-data-' + idx" v-bind:class="getCss(idx)"
+				v-bind:aria-labelledby="'item-image-group-item-' + idx" role="tabpanel" class="card-block collapse row">
+
+				<div class="col-xl-6">
+
+					<div class="form-group row image-preview">
+						<input class="item-preview" type="hidden"
+							name="<?= $enc->attr( $this->formparam( array( 'image', 'media.preview', '' ) ) ); ?>"
+							v-model="items['media.preview'][idx]" />
+						<input v-on:change="updateFile(idx, $event.target.files)"
+							class="fileupload" type="file" name="image[files][]" tabindex="<?= $this->get( 'tabindex' ); ?>" />
+						<img v-if="items['media.preview'][idx]" class="item-preview"
+							v-bind:src="getUrl('<?= $this->content('') ?>', items['media.preview'][idx])"
+							v-bind:alt="items['media.label'][idx]" />
+						<p v-else class="item-preview">
+							{{ items['media.label'][idx] || '<?= $enc->html( $this->translate( 'admin', 'Select file' ) ) ?>' }}
+						</p>
+					</div>
+
+				</div>
+
+				<div class="col-xl-6">
+
+					<div class="form-group row mandatory">
+						<label class="col-sm-4 form-control-label"><?= $enc->html( $this->translate( 'admin', 'Status' ) ); ?></label>
+						<div class="col-sm-8">
+							<select class="form-control custom-select item-status" required="required" tabindex="<?= $this->get( 'tabindex' ); ?>"
+								name="<?= $enc->attr( $this->formparam( array( 'image', 'media.status', '' ) ) ); ?>"
+								v-bind:readonly="checkSite('media.siteid', idx)"
+								v-model="items['media.status'][idx]" >
+								<option value="1" v-bind:selected="items['media.status'][idx] == 1" >
+									<?= $enc->html( $this->translate( 'mshop/code', 'status:1' ) ); ?>
+								</option>
+								<option value="0" v-bind:selected="items['media.status'][idx] == 0" >
+									<?= $enc->html( $this->translate( 'mshop/code', 'status:0' ) ); ?>
+								</option>
+								<option value="-1" v-bind:selected="items['media.status'][idx] == -1" >
+									<?= $enc->html( $this->translate( 'mshop/code', 'status:-1' ) ); ?>
+								</option>
+								<option value="-2" v-bind:selected="items['media.status'][idx] == -2" >
+									<?= $enc->html( $this->translate( 'mshop/code', 'status:-2' ) ); ?>
+								</option>
+							</select>
+						</div>
+					</div>
+					<?php $mediaTypes = $this->get( 'imageTypes', [] ); ?>
+					<?php if( count( $mediaTypes ) > 1 ) : ?>
+						<div class="form-group row mandatory">
+							<label class="col-sm-4 form-control-label help"><?= $enc->html( $this->translate( 'admin', 'Type' ) ); ?></label>
+							<div class="col-sm-8">
+								<select class="form-control custom-select item-typeid" required="required" tabindex="<?= $this->get( 'tabindex' ); ?>"
+									name="<?= $enc->attr( $this->formparam( array( 'image', 'media.typeid', '' ) ) ); ?>"
+									data-default="<?= $enc->attr( key( $mediaTypes ) ) ?>"
+									v-bind:readonly="checkSite('media.siteid', idx)"
+									v-model="items['media.typeid'][idx]" >
+
+									<option value="" disabled >
+										<?= $enc->attr( $this->translate( 'admin', 'Please select' ) ); ?>
+									</option>
+
+									<?php foreach( (array) $mediaTypes as $typeId => $typeItem ) : ?>
+										<option value="<?= $enc->attr( $typeId ); ?>" v-bind:selected="items['media.typeid'][idx] == '<?= $enc->attr( $typeId ) ?>'" >
+											<?= $enc->html( $typeItem->getLabel() ); ?>
+										</option>
+									<?php endforeach; ?>
+								</select>
+							</div>
+							<div class="col-sm-12 form-text text-muted help-text">
+								<?= $enc->html( $this->translate( 'admin', 'Types for additional images like icons' ) ); ?>
+							</div>
+						</div>
+					<?php else : ?>
+						<input class="item-typeid" type="hidden"
+							name="<?= $enc->attr( $this->formparam( array( 'image', 'media.typeid', '' ) ) ); ?>"
+							value="<?= $enc->attr( key( $mediaTypes ) ); ?>" />
+					<?php endif; ?>
+
+					<div class="form-group row mandatory">
+							<label class="col-sm-4 form-control-label help"><?= $enc->html( $this->translate( 'admin', 'Title' ) ); ?></label>
+						<div class="col-sm-8">
+							<input class="form-control item-label" type="text" required="required" tabindex="<?= $this->get( 'tabindex' ); ?>"
+								name="<?= $enc->attr( $this->formparam( array( 'image', 'media.label', '' ) ) ); ?>"
+								placeholder="<?= $enc->attr( $this->translate( 'admin', 'Title' ) ); ?>"
+								v-bind:readonly="checkSite('media.siteid', idx)"
+								v-model="items['media.label'][idx]" />
+						</div>
+						<div class="col-sm-12 form-text text-muted help-text">
+							<?= $enc->html( $this->translate( 'admin', 'The image title is used for the title tag of the image on the web site' ) ); ?>
+						</div>
+					</div>
+
+					<div class="form-group row optional">
+						<label class="col-sm-4 form-control-label help"><?= $enc->html( $this->translate( 'admin', 'Language' ) ); ?></label>
+						<div class="col-sm-8">
+							<select class="form-control custom-select item-languageid" tabindex="<?= $this->get( 'tabindex' ); ?>"
+								name="<?= $enc->attr( $this->formparam( array( 'image', 'media.languageid', '' ) ) ); ?>"
+								v-bind:readonly="checkSite('media.siteid', idx)"
+								v-model="items['media.languageid'][idx]" >
+
+								<option v-bind:value="null">
+									<?= $enc->html( $this->translate( 'admin', 'All' ) ); ?>
+								</option>
+
+								<?php foreach( $this->get( 'pageLangItems', [] ) as $langId => $langItem ) : ?>
+									<option value="<?= $enc->attr( $langId ); ?>" v-bind:selected="items['media.languageid'][idx] == '<?= $enc->attr( $langId ) ?>'" >
+										<?= $enc->html( $langItem->getLabel() ); ?>
+									</option>
+								<?php endforeach; ?>
+							</select>
+						</div>
+						<div class="col-sm-12 form-text text-muted help-text">
+							<?= $enc->html( $this->translate( 'admin', 'Images will only be shown for that language, useful if the image contains text or is language sepecific' ) ); ?>
+						</div>
+					</div>
+
+				</div>
+
+
+				<div v-on:click="toggle(idx)" class="col-xl-12 advanced" v-bind:class="{ 'collapsed': !advanced[idx] }">
+					<div class="card-tools-left">
+						<div class="btn act-show fa" tabindex="<?= $this->get( 'tabindex' ); ?>"
+							title="<?= $enc->attr( $this->translate( 'admin', 'Show/hide advanced data') ); ?>">
+						</div>
+					</div>
+					<span class="header-label"><?= $enc->html( $this->translate( 'admin', 'Advanced' ) ); ?></span>
+				</div>
+
+				<div v-show="advanced[idx]" class="col-xl-6 content-block secondary">
+					<?php $listTypes = $this->get( 'imageListTypes', [] ); ?>
+					<?php if( count( $listTypes ) > 1 ) : ?>
+						<div class="form-group row mandatory">
+							<label class="col-sm-4 form-control-label help"><?= $enc->html( $this->translate( 'admin', 'List type' ) ); ?></label>
+							<div class="col-sm-8">
+								<select class="form-control custom-select listitem-typeid" required="required" tabindex="<?= $this->get( 'tabindex' ); ?>"
+									name="<?= $enc->attr( $this->formparam( array( 'image', 'supplier.lists.typeid', '' ) ) ); ?>"
+									v-bind:readonly="checkSite('supplier.lists.siteid', idx)"
+									v-model="items['supplier.lists.typeid'][idx]" >
+
+									<?php foreach( $this->get( 'imageListTypes', [] ) as $id => $typeItem ) : ?>
+										<option value="<?= $enc->attr( $id ); ?>" v-bind:selected="items['supplier.lists.typeid'][idx] == '<?= $enc->attr( $id ) ?>'" >
+											<?= $enc->html( $typeItem->getLabel() ); ?>
+										</option>
+									<?php endforeach; ?>
+								</select>
+							</div>
+							<div class="col-sm-12 form-text text-muted help-text">
+								<?= $enc->html( $this->translate( 'admin', 'Second level type for grouping items' ) ); ?>
+							</div>
+						</div>
+					<?php else : ?>
+						<input class="listitem-typeid" type="hidden"
+							name="<?= $enc->attr( $this->formparam( array( 'image', 'supplier.lists.typeid', '' ) ) ); ?>"
+							value="<?= $enc->attr( key( $listTypes ) ); ?>"
+							v-model="items['supplier.lists.typeid'][idx]" />
+					<?php endif; ?>
+					<div class="form-group row optional">
+						<label class="col-sm-4 form-control-label help"><?= $enc->html( $this->translate( 'admin', 'Start date' ) ); ?></label>
+						<div class="col-sm-8">
+							<input class="form-control listitem-datestart" type="datetime-local" tabindex="<?= $this->get( 'tabindex' ); ?>"
+								name="<?= $enc->attr( $this->formparam( array( 'image', 'supplier.lists.datestart', '' ) ) ); ?>"
+								placeholder="<?= $enc->attr( $this->translate( 'admin', 'YYYY-MM-DD hh:mm:ss (optional)' ) ); ?>"
+								v-bind:readonly="checkSite('supplier.lists.siteid', idx)"
+								v-model="items['supplier.lists.datestart'][idx]" />
+						</div>
+						<div class="col-sm-12 form-text text-muted help-text">
+							<?= $enc->html( $this->translate( 'admin', 'The item is only shown on the web site after that date and time' ) ); ?>
+						</div>
+					</div>
+					<div class="form-group row optional">
+						<label class="col-sm-4 form-control-label help"><?= $enc->html( $this->translate( 'admin', 'End date' ) ); ?></label>
+						<div class="col-sm-8">
+							<input class="form-control listitem-dateend" type="datetime-local" tabindex="<?= $this->get( 'tabindex' ); ?>"
+								name="<?= $enc->attr( $this->formparam( array( 'image', 'supplier.lists.dateend', '' ) ) ); ?>"
+								placeholder="<?= $enc->attr( $this->translate( 'admin', 'YYYY-MM-DD hh:mm:ss (optional)' ) ); ?>"
+								v-bind:readonly="checkSite('supplier.lists.siteid', idx)"
+								v-model="items['supplier.lists.dateend'][idx]" />
+						</div>
+						<div class="col-sm-12 form-text text-muted help-text">
+							<?= $enc->html( $this->translate( 'admin', 'The item is only shown on the web site until that date and time' ) ); ?>
+						</div>
+					</div>
+				</div>
+
+				<div v-show="advanced[idx]" class="col-xl-6 content-block secondary" v-bind:class="items['supplier.lists.siteid'][idx] != '<?= $this->site()->siteid() ?>' ? 'readonly' : ''">
+					<table class="item-config table table-striped">
+						<thead>
+							<tr>
+								<th>
+									<span class="help"><?= $enc->html( $this->translate( 'admin', 'Option' ) ); ?></span>
+									<div class="form-text text-muted help-text">
+										<?= $enc->html( $this->translate( 'admin', 'Configuration options, will be available as key/value pairs in the list item' ) ); ?>
+									</div>
+								</th>
+								<th>
+									<?= $enc->html( $this->translate( 'admin', 'Value' ) ); ?>
+								</th>
+								<th class="actions">
+									<div class="btn act-add fa" tabindex="<?= $this->get( 'tabindex' ); ?>"
+										title="<?= $enc->attr( $this->translate( 'admin', 'Insert new entry (Ctrl+I)') ); ?>"
+										v-bind:readonly="checkSite('supplier.lists.siteid', idx)"
+										v-on:click="addConfig(idx)" >
+									</div>
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+
+							<tr v-for="(key, pos) in getConfig(idx)" v-bind:key="pos">
+								<td>
+									<input is="auto-complete" v-once
+										v-model="items['config'][idx]['key'][pos]"
+										v-bind:value="items['config'][idx]['key'][pos]"
+										v-bind:name="'<?= $enc->attr( $this->formparam( array( 'image', 'config', 'idx', 'key', '' ) ) ); ?>'.replace('idx', idx)"
+										v-bind:readonly="checkSite('supplier.lists.siteid', idx)"
+										v-bind:tabindex="<?= $this->get( 'tabindex' ); ?>"
+										v-bind:keys="[]" />
+								</td>
+								<td>
+									<input type="text" class="form-control" tabindex="<?= $this->get( 'tabindex' ); ?>"
+										v-bind:name="'<?= $enc->attr( $this->formparam( array( 'image', 'config', 'idx', 'val', '' ) ) ); ?>'.replace('idx', idx)"
+										v-bind:readonly="checkSite('supplier.lists.siteid', idx)"
+										v-model="items['config'][idx]['val'][pos]" />
+								</td>
+								<td class="actions">
+									<div v-if="!checkSite('supplier.lists.siteid', idx)" v-on:click="removeConfig(idx, pos)"
+										class="btn act-delete fa" tabindex="<?= $this->get( 'tabindex' ); ?>"
+										title="<?= $enc->attr( $this->translate( 'admin', 'Delete this entry') ); ?>">
+									</div>
+								</td>
+							</tr>
+
+						</tbody>
+					</table>
+				</div>
+
+				<?= $this->get( 'imageBody' ); ?>
+
+			</div>
+		</div>
+	</div>
+
+	<div class="card-tools-more">
+		<div class="btn btn-primary btn-card-more act-add fa" tabindex="<?= $this->get( 'tabindex' ); ?>"
+			title="<?= $enc->attr( $this->translate( 'admin', 'Insert new entry (Ctrl+I)') ); ?>"
+			v-on:click="addItem('supplier.lists.')" >
+		</div>
+	</div>
+</div>
---
+++ admin/jqadm/tests/Admin/JQAdm/Supplier/Image/StandardTest.php
@@ -0,0 +1,210 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2018
+ */
+
+
+namespace Aimeos\Admin\JQAdm\Supplier\Image;
+
+
+class StandardTest extends \PHPUnit\Framework\TestCase
+{
+	private $context;
+	private $object;
+	private $view;
+
+
+	protected function setUp()
+	{
+		$this->view = \TestHelperJqadm::getView();
+		$this->context = \TestHelperJqadm::getContext();
+
+		$this->object = new \Aimeos\Admin\JQAdm\Supplier\Image\Standard( $this->context );
+		$this->object->setAimeos( \TestHelperJqadm::getAimeos() );
+		$this->object->setView( $this->view );
+	}
+
+
+	protected function tearDown()
+	{
+		unset( $this->object, $this->view, $this->context );
+	}
+
+
+	public function testCreate()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'supplier' );
+
+		$this->view->item = $manager->createItem();
+		$result = $this->object->create();
+
+		$this->assertContains( 'item-image', $result );
+		$this->assertNull( $this->view->get( 'errors' ) );
+	}
+
+
+	public function testCopy()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'supplier' );
+
+		$this->view->item = $manager->findItem( 'unitCode001', array( 'media' ) );
+		$result = $this->object->copy();
+
+		$this->assertNull( $this->view->get( 'errors' ) );
+		$this->assertRegexp( '/&quot;media.preview&quot;:\[.*supplier\.jpg&quot;.*\]/', $result );
+	}
+
+
+	public function testDelete()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'supplier' );
+
+		$this->view->item = $manager->createItem();
+		$result = $this->object->delete();
+
+		$this->assertNull( $this->view->get( 'errors' ) );
+		$this->assertNull( $result );
+	}
+
+
+	public function testGet()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'supplier' );
+
+		$this->view->item = $manager->findItem( 'unitCode001', array( 'media' ) );
+		$result = $this->object->get();
+
+		$this->assertNull( $this->view->get( 'errors' ) );
+		$this->assertRegexp( '/&quot;media.preview&quot;:\[.*supplier\.jpg&quot;.*\]/', $result );
+	}
+
+
+	public function testSave()
+	{
+		$listTypeManager = \Aimeos\MShop\Factory::createManager( $this->context, 'supplier/lists/type' );
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'supplier' );
+
+		$item = $manager->findItem( 'unitCode001' );
+		$item->setCode( 'jqadm-test-image' );
+		$item->setId( null );
+
+		$item = $manager->saveItem( $item );
+
+
+		$param = array(
+			'site' => 'unittest',
+			'image' => array(
+				'supplier.lists.id' => array( '' ),
+				'supplier.lists.typeid' => array( $listTypeManager->findItem( 'default', [], 'media' )->getId() ),
+				'media.languageid' => array( 'de' ),
+				'media.label' => array( 'test' ),
+			),
+		);
+
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $param );
+		$this->view->addHelper( 'param', $helper );
+
+		$file = $this->getMockBuilder( '\Psr\Http\Message\UploadedFileInterface' )->getMock();
+		$request = $this->getMockBuilder( '\Psr\Http\Message\ServerRequestInterface' )->getMock();
+		$request->expects( $this->any() )->method( 'getUploadedFiles' )
+			->will( $this->returnValue( array( 'image' => array( 'files' => array( $file ) ) ) ) );
+
+		$helper = new \Aimeos\MW\View\Helper\Request\Standard( $this->view, $request, '127.0.0.1', 'test' );
+		$this->view ->addHelper( 'request', $helper );
+
+		$this->view->item = $item;
+
+
+		$name = 'AdminJQAdmSupplierImageSave';
+		$this->context->getConfig()->set( 'controller/common/media/name', $name );
+
+		$cntlStub = $this->getMockBuilder( '\\Aimeos\\Controller\\Common\\Media\\Standard' )
+			->setConstructorArgs( array( $this->context ) )
+			->setMethods( array( 'add' ) )
+			->getMock();
+
+		\Aimeos\Controller\Common\Media\Factory::injectController( '\\Aimeos\\Controller\\Common\\Media\\' . $name, $cntlStub );
+
+		$cntlStub->expects( $this->once() )->method( 'add' );
+
+
+		$mediaStub = $this->getMockBuilder( '\\Aimeos\\MShop\\Media\\Manager\\Standard' )
+			->setConstructorArgs( array( $this->context ) )
+			->setMethods( array( 'saveItem' ) )
+			->getMock();
+
+		\Aimeos\MShop\Factory::injectManager( $this->context, 'media', $mediaStub );
+
+		$mediaStub->expects( $this->once() )->method( 'saveItem' )
+			->will( $this->returnValue( $mediaStub->createItem() ) );
+
+
+		\Aimeos\MShop\Factory::setCache( true );
+
+		$result = $this->object->save();
+
+		\Aimeos\MShop\Factory::setCache( false );
+
+		$item = $manager->getItem( $item->getId(), array( 'media' ) );
+		$manager->deleteItem( $item->getId() );
+
+		$this->assertNull( $this->view->get( 'errors' ) );
+		$this->assertNull( $result );
+		$this->assertEquals( 1, count( $item->getListItems( 'media' ) ) );
+	}
+
+
+	public function testSaveException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Supplier\Image\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'fromArray' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'fromArray' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$this->view = \TestHelperJqadm::getView();
+		$this->view->item = \Aimeos\MShop\Factory::createManager( $this->context, 'supplier' )->createItem();
+
+		$object->setView( $this->view );
+
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$object->save();
+	}
+
+
+	public function testSaveMShopException()
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Supplier\Image\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( array( 'fromArray' ) )
+			->getMock();
+
+		$object->expects( $this->once() )->method( 'fromArray' )
+			->will( $this->throwException( new \Aimeos\MShop\Exception() ) );
+
+		$this->view = \TestHelperJqadm::getView();
+		$this->view->item = \Aimeos\MShop\Factory::createManager( $this->context, 'supplier' )->createItem();
+
+		$object->setView( $this->view );
+
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$object->save();
+	}
+
+
+	public function testSearch()
+	{
+		$this->assertNull( $this->object->search() );
+	}
+
+
+	public function testGetSubClient()
+	{
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$this->object->getSubClient( 'unknown' );
+	}
+}
