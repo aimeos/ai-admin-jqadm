97ad7cbfb13203174dba7e19fcfb7ae4b75c16f4
Aimeos <aimeos@aimeos.org>
2018-03-01 17:25:32 +0100

    Implements subscription panel

---
+++ admin/jqadm/templates/subscription/list-standard.php
@@ -0,0 +1,207 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2018
+ */
+
+$enc = $this->encoder();
+
+
+$target = $this->config( 'admin/jqadm/url/search/target' );
+$controller = $this->config( 'admin/jqadm/url/search/controller', 'Jqadm' );
+$action = $this->config( 'admin/jqadm/url/search/action', 'search' );
+$config = $this->config( 'admin/jqadm/url/search/config', [] );
+
+$newTarget = $this->config( 'admin/jqadm/url/create/target' );
+$newCntl = $this->config( 'admin/jqadm/url/create/controller', 'Jqadm' );
+$newAction = $this->config( 'admin/jqadm/url/create/action', 'create' );
+$newConfig = $this->config( 'admin/jqadm/url/create/config', [] );
+
+$getTarget = $this->config( 'admin/jqadm/url/get/target' );
+$getCntl = $this->config( 'admin/jqadm/url/get/controller', 'Jqadm' );
+$getAction = $this->config( 'admin/jqadm/url/get/action', 'get' );
+$getConfig = $this->config( 'admin/jqadm/url/get/config', [] );
+
+$copyTarget = $this->config( 'admin/jqadm/url/copy/target' );
+$copyCntl = $this->config( 'admin/jqadm/url/copy/controller', 'Jqadm' );
+$copyAction = $this->config( 'admin/jqadm/url/copy/action', 'copy' );
+$copyConfig = $this->config( 'admin/jqadm/url/copy/config', [] );
+
+$delTarget = $this->config( 'admin/jqadm/url/delete/target' );
+$delCntl = $this->config( 'admin/jqadm/url/delete/controller', 'Jqadm' );
+$delAction = $this->config( 'admin/jqadm/url/delete/action', 'delete' );
+$delConfig = $this->config( 'admin/jqadm/url/delete/config', [] );
+
+
+/** admin/jqadm/subscription/fields
+ * List of subscription columns that should be displayed in the list view
+ *
+ * Changes the list of subscription columns shown by default in the subscription list view.
+ * The columns can be changed by the editor as required within the administraiton
+ * interface.
+ *
+ * The names of the colums are in fact the search keys defined by the managers,
+ * e.g. "subscription.id" for the subscription ID.
+ *
+ * @param array List of field names, i.e. search keys
+ * @since 2018.04
+ * @category Developer
+ */
+$default = ['subscription.status', 'subscription.datenext', 'subscription.dateend', 'subscription.interval'];
+$default = $this->config( 'admin/jqadm/subscription/fields', $default );
+$fields = $this->session( 'aimeos/admin/jqadm/subscription/fields', $default );
+
+$searchParams = $params = $this->get( 'pageParams', [] );
+$searchParams['page']['start'] = 0;
+
+$columnList = [
+	'subscription.id' => $this->translate( 'admin', 'ID' ),
+	'subscription.status' => $this->translate( 'admin', 'Status' ),
+	'subscription.datenext' => $this->translate( 'admin', 'Next date' ),
+	'subscription.dateend' => $this->translate( 'admin', 'End date' ),
+	'subscription.interval' => $this->translate( 'admin', 'Interval' ),
+	'subscription.ctime' => $this->translate( 'admin', 'Created' ),
+	'subscription.mtime' => $this->translate( 'admin', 'Modified' ),
+	'subscription.editor' => $this->translate( 'admin', 'Editor' ),
+];
+
+
+?>
+<?php $this->block()->start( 'jqadm_content' ); ?>
+
+<nav class="main-navbar">
+
+	<span class="navbar-brand">
+		<?= $enc->html( $this->translate( 'admin', 'Subscription' ) ); ?>
+		<span class="navbar-secondary">(<?= $enc->html( $this->site()->label() ); ?>)</span>
+	</span>
+
+	<?= $this->partial(
+		$this->config( 'admin/jqadm/partial/navsearch', 'common/partials/navsearch-standard.php' ), [
+			'filter' => $this->session( 'aimeos/admin/jqadm/subscription/filter', [] ),
+			'filterAttributes' => $this->get( 'filterAttributes', [] ),
+			'filterOperators' => $this->get( 'filterOperators', [] ),
+			'params' => $params,
+		]
+	); ?>
+</nav>
+
+
+<?= $this->partial(
+		$this->config( 'admin/jqadm/partial/pagination', 'common/partials/pagination-standard.php' ),
+		['pageParams' => $params, 'pos' => 'top', 'total' => $this->get( 'total' ),
+		'page' => $this->session( 'aimeos/admin/jqadm/subscription/page', [] )]
+	);
+?>
+
+<form class="list list-subscription" method="POST" action="<?= $enc->attr( $this->url( $target, $controller, $action, $searchParams, [], $config ) ); ?>">
+	<?= $this->csrf()->formfield(); ?>
+
+	<table class="list-items table table-hover table-striped">
+		<thead class="list-header">
+			<tr>
+
+				<?= $this->partial(
+						$this->config( 'admin/jqadm/partial/listhead', 'common/partials/listhead-standard.php' ),
+						['fields' => $fields, 'params' => $params, 'data' => $columnList, 'sort' => $this->session( 'aimeos/admin/jqadm/subscription/sort' )]
+					);
+				?>
+
+				<th class="actions">
+					<a class="btn fa act-add" tabindex="1"
+						href="<?= $enc->attr( $this->url( $newTarget, $newCntl, $newAction, $params, [], $newConfig ) ); ?>"
+						title="<?= $enc->attr( $this->translate( 'admin', 'Insert new entry (Ctrl+I)') ); ?>"
+						aria-label="<?= $enc->attr( $this->translate( 'admin', 'Add' ) ); ?>">
+					</a>
+
+					<?= $this->partial(
+							$this->config( 'admin/jqadm/partial/columns', 'common/partials/columns-standard.php' ),
+							['fields' => $fields, 'data' => $columnList]
+						);
+					?>
+				</th>
+			</tr>
+		</thead>
+		<tbody>
+
+			<?= $this->partial(
+				$this->config( 'admin/jqadm/partial/listsearch', 'common/partials/listsearch-standard.php' ), [
+					'fields' => $fields, 'filter' => $this->session( 'aimeos/admin/jqadm/subscription/filter', [] ),
+					'data' => [
+						'subscription.id' => ['op' => '=='],
+						'subscription.status' => ['op' => '==', 'type' => 'select', 'val' => [
+							'1' => $this->translate( 'mshop/code', 'status:1' ),
+							'0' => $this->translate( 'mshop/code', 'status:0' ),
+							'-1' => $this->translate( 'mshop/code', 'status:-1' ),
+							'-2' => $this->translate( 'mshop/code', 'status:-2' ),
+						]],
+						'subscription.datenext' => ['op' => '>=', 'type' => 'date'],
+						'subscription.dateend' => ['op' => '>=', 'type' => 'date'],
+						'subscription.ctime' => ['op' => '>=', 'type' => 'datetime-local'],
+						'subscription.mtime' => ['op' => '>=', 'type' => 'datetime-local'],
+						'subscription.editor' => [],
+					]
+				] );
+			?>
+
+			<?php foreach( $this->get( 'items', [] ) as $id => $item ) : ?>
+				<?php $url = $enc->attr( $this->url( $getTarget, $getCntl, $getAction, ['id' => $id] + $params, [], $getConfig ) ); ?>
+				<tr class="<?= $this->site()->readonly( $item->getSiteId() ); ?>">
+					<?php if( in_array( 'subscription.id', $fields ) ) : ?>
+						<td class="subscription-id"><a class="items-field" href="<?= $url; ?>"><?= $enc->html( $item->getId() ); ?></a></td>
+					<?php endif; ?>
+					<?php if( in_array( 'subscription.status', $fields ) ) : ?>
+						<td class="subscription-status"><a class="items-field" href="<?= $url; ?>"><div class="fa status-<?= $enc->attr( $item->getStatus() ); ?>"></div></a></td>
+					<?php endif; ?>
+					<?php if( in_array( 'subscription.interval', $fields ) ) : ?>
+						<td class="subscription-interval"><a class="items-field" href="<?= $url; ?>"><?= $enc->html( $item->getInterval() ); ?></a></td>
+					<?php endif; ?>
+					<?php if( in_array( 'subscription.datenext', $fields ) ) : ?>
+						<td class="subscription-datenext"><a class="items-field" href="<?= $url; ?>" tabindex="1"><?= $enc->html( $item->getDateNext() ); ?></a></td>
+					<?php endif; ?>
+					<?php if( in_array( 'subscription.dateend', $fields ) ) : ?>
+						<td class="subscription-dateend"><a class="items-field" href="<?= $url; ?>"><?= $enc->html( $item->getDateEnd() ); ?></a></td>
+					<?php endif; ?>
+					<?php if( in_array( 'subscription.ctime', $fields ) ) : ?>
+						<td class="subscription-ctime"><a class="items-field" href="<?= $url; ?>"><?= $enc->html( $item->getTimeCreated() ); ?></a></td>
+					<?php endif; ?>
+					<?php if( in_array( 'subscription.mtime', $fields ) ) : ?>
+						<td class="subscription-mtime"><a class="items-field" href="<?= $url; ?>"><?= $enc->html( $item->getTimeModified() ); ?></a></td>
+					<?php endif; ?>
+					<?php if( in_array( 'subscription.editor', $fields ) ) : ?>
+						<td class="subscription-editor"><a class="items-field" href="<?= $url; ?>"><?= $enc->html( $item->getEditor() ); ?></a></td>
+					<?php endif; ?>
+
+					<td class="actions">
+						<a class="btn act-copy fa" tabindex="1"
+							href="<?= $enc->attr( $this->url( $copyTarget, $copyCntl, $copyAction, ['id' => $id] + $params, [], $copyConfig ) ); ?>"
+							title="<?= $enc->attr( $this->translate( 'admin', 'Copy this entry') ); ?>"
+							aria-label="<?= $enc->attr( $this->translate( 'admin', 'Copy' ) ); ?>"></a>
+						<?php if( !$this->site()->readonly( $item->getSiteId() ) ) : ?>
+							<a class="btn act-delete fa" tabindex="1"
+								href="<?= $enc->attr( $this->url( $delTarget, $delCntl, $delAction, ['resource' => 'subscription', 'id' => $id] + $params, [], $delConfig ) ); ?>"
+								title="<?= $enc->attr( $this->translate( 'admin', 'Delete this entry') ); ?>"
+								aria-label="<?= $enc->attr( $this->translate( 'admin', 'Delete' ) ); ?>"></a>
+						<?php endif; ?>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+
+	<?php if( $this->get( 'items', [] ) === [] ) : ?>
+		<div class="noitems"><?= $enc->html( sprintf( $this->translate( 'admin', 'No items found' ) ) ); ?></div>
+	<?php endif; ?>
+</form>
+
+<?= $this->partial(
+		$this->config( 'admin/jqadm/partial/pagination', 'common/partials/pagination-standard.php' ),
+		['pageParams' => $params, 'pos' => 'bottom', 'total' => $this->get( 'total' ),
+		'page' => $this->session( 'aimeos/admin/jqadm/subscription/page', [] )]
+	);
+?>
+
+<?php $this->block()->stop(); ?>
+
+<?= $this->render( $this->config( 'admin/jqadm/template/page', 'common/page-standard.php' ) ); ?>
---
+++ admin/jqadm/tests/Admin/JQAdm/Subscription/StandardTest.php
@@ -0,0 +1,336 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2018
+ */
+
+
+namespace Aimeos\Admin\JQAdm\Subscription;
+
+
+class StandardTest extends \PHPUnit\Framework\TestCase
+{
+	private $context;
+	private $object;
+	private $view;
+
+
+	protected function setUp()
+	{
+		$this->view = \TestHelperJqadm::getView();
+		$this->context = \TestHelperJqadm::getContext();
+
+		$this->object = new \Aimeos\Admin\JQAdm\Subscription\Standard( $this->context );
+		$this->object = new \Aimeos\Admin\JQAdm\Common\Decorator\Page( $this->object, $this->context );
+		$this->object->setAimeos( \TestHelperJqadm::getAimeos() );
+		$this->object->setView( $this->view );
+	}
+
+
+	protected function tearDown()
+	{
+		unset( $this->object, $this->view, $this->context );
+	}
+
+
+	public function testCreate()
+	{
+		$this->object->create();
+	}
+
+
+	public function testCreateException()
+	{
+		$object = $this->getClientMock( 'getSubClients' );
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->create();
+	}
+
+
+	public function testCreateMShopException()
+	{
+		$object = $this->getClientMock( 'getSubClients' );
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \Aimeos\MShop\Exception() ) );
+
+		$object->create();
+	}
+
+
+	public function testCopy()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'subscription' );
+
+		$param = ['site' => 'unittest', 'id' => $this->getSubscriptionId()];
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $param );
+		$this->view->addHelper( 'param', $helper );
+
+		$result = $this->object->copy();
+
+		$this->assertContains( '2010-01-01 00:00:00', $result );
+	}
+
+
+	public function testCopyException()
+	{
+		$object = $this->getClientMock( 'getSubClients' );
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->copy();
+	}
+
+
+	public function testCopyMShopException()
+	{
+		$object = $this->getClientMock( 'getSubClients' );
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \Aimeos\MShop\Exception() ) );
+
+		$object->copy();
+	}
+
+
+	public function testDelete()
+	{
+		$this->assertNotNull( $this->object->delete() );
+	}
+
+
+	public function testDeleteException()
+	{
+		$object = $this->getClientMock( 'getSubClients' );
+
+		$object->expects( $this->exactly( 2 ) )->method( 'getSubClients' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->delete();
+	}
+
+
+	public function testDeleteMShopException()
+	{
+		$object = $this->getClientMock( 'getSubClients' );
+
+		$object->expects( $this->exactly( 2 ) )->method( 'getSubClients' )
+			->will( $this->throwException( new \Aimeos\MShop\Exception() ) );
+
+		$object->delete();
+	}
+
+
+	public function testGet()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'subscription' );
+
+		$param = ['site' => 'unittest', 'id' => $this->getSubscriptionId()];
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $param );
+		$this->view->addHelper( 'param', $helper );
+
+		$result = $this->object->get();
+
+		$this->assertContains( '2010-01-01 00:00:00', $result );
+	}
+
+
+	public function testGetException()
+	{
+		$object = $this->getClientMock( 'getSubClients' );
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->get();
+	}
+
+
+	public function testGetMShopException()
+	{
+		$object = $this->getClientMock( 'getSubClients' );
+
+		$object->expects( $this->once() )->method( 'getSubClients' )
+			->will( $this->throwException( new \Aimeos\MShop\Exception() ) );
+
+		$object->get();
+	}
+
+
+	public function testGetViewException()
+	{
+		$object = new \Aimeos\Admin\JQAdm\Subscription\Standard( $this->context, [] );
+
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$object->getView();
+	}
+
+
+	public function testSave()
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'subscription' );
+
+		$param = array(
+			'site' => 'unittest',
+			'item' => array(
+				'subscription.id' => '',
+				'subscription.ordbaseid' => '12',
+				'subscription.ordprodid' => '123',
+				'subscription.interval' => 'P100Y100M100W100D',
+				'subscription.datenext' => '2005-05-05 05:05:05',
+				'subscription.dateend' => '2006-06-06 06:06:06',
+				'subscription.status' => '1',
+			),
+		);
+
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $param );
+		$this->view->addHelper( 'param', $helper );
+
+		$this->object->save();
+
+		$manager->deleteItem( $this->getSubscriptionId( '2006-06-06 06:06:06' ) );
+	}
+
+
+	public function testSaveException()
+	{
+		$object = $this->getClientMock( 'fromArray' );
+
+		$object->expects( $this->once() )->method( 'fromArray' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->save();
+	}
+
+
+	public function testSaveMShopException()
+	{
+		$object = $this->getClientMock( 'fromArray' );
+
+		$object->expects( $this->once() )->method( 'fromArray' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->save();
+	}
+
+
+	public function testSaveJQAdmException()
+	{
+		$object = $this->getClientMock( 'fromArray' );
+
+		$object->expects( $this->once() )->method( 'fromArray' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->save();
+	}
+
+
+	public function testSearch()
+	{
+		$param = array(
+			'site' => 'unittest', 'lang' => 'de',
+			'filter' => array(
+				'key' => array( 0 => 'subscription.dateend' ),
+				'op' => array( 0 => '>=' ),
+				'val' => array( 0 => '2010-01-01 00:00:00' ),
+			),
+			'sort' => array( 'subscription.datenext', '-subscription.id' ),
+		);
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $param );
+		$this->view->addHelper( 'param', $helper );
+
+		$result = $this->object->search();
+
+		$this->assertContains( '2010-01-01 00:00:00', $result );
+	}
+
+
+	public function testSearchException()
+	{
+		$object = $this->getClientMock( 'initCriteria' );
+
+		$object->expects( $this->once() )->method( 'initCriteria' )
+			->will( $this->throwException( new \RuntimeException() ) );
+
+		$object->search();
+	}
+
+
+	public function testSearchMShopException()
+	{
+		$object = $this->getClientMock( 'initCriteria' );
+
+		$object->expects( $this->once() )->method( 'initCriteria' )
+			->will( $this->throwException( new \Aimeos\MShop\Exception() ) );
+
+		$object->search();
+	}
+
+
+	public function testGetSubClientInvalid()
+	{
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$this->object->getSubClient( '$unknown$' );
+	}
+
+
+	public function testGetSubClientUnknown()
+	{
+		$this->setExpectedException( '\Aimeos\Admin\JQAdm\Exception' );
+		$this->object->getSubClient( 'unknown' );
+	}
+
+
+	public function getClientMock( $method )
+	{
+		$object = $this->getMockBuilder( '\Aimeos\Admin\JQAdm\Subscription\Standard' )
+			->setConstructorArgs( array( $this->context, \TestHelperJqadm::getTemplatePaths() ) )
+			->setMethods( [$method] )
+			->getMock();
+
+		$object->setAimeos( \TestHelperJqadm::getAimeos() );
+		$object->setView( $this->getViewNoRender() );
+
+		return $object;
+	}
+
+
+	protected function getViewNoRender()
+	{
+		$view = $this->getMockBuilder( '\Aimeos\MW\View\Standard' )
+			->setConstructorArgs( array( [] ) )
+			->setMethods( array( 'render', 'config' ) )
+			->getMock();
+
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'subscription' );
+
+		$param = ['site' => 'unittest', 'id' => $this->getSubscriptionId()];
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $view, $param );
+		$view->addHelper( 'param', $helper );
+
+		$helper = new \Aimeos\MW\View\Helper\Access\Standard( $view, [] );
+		$view->addHelper( 'access', $helper );
+
+		return $view;
+	}
+
+
+	protected function getSubscriptionId( $end = '2010-01-01 00:00:00' )
+	{
+		$manager = \Aimeos\MShop\Factory::createManager( $this->context, 'subscription' );
+		$search = $manager->createSearch()->setSlice( 0, 1 );
+		$search->setConditions( $search->compare( '==', 'subscription.dateend', $end ) );
+		$items = $manager->searchItems( $search );
+
+		if( ( $item = reset( $items ) ) === false ) {
+			throw new \Exception( sprintf( 'No subscription item found for end date "%1$s"', $end ) );
+		}
+
+		return $item->getId();
+	}
+}
