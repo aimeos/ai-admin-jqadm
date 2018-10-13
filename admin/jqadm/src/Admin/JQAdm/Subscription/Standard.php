1d5ed6d20771a96412a8e3ff60b12b7915b78ece
Aimeos <aimeos@aimeos.org>
2018-10-13 15:51:33 +0200

    Adds order product details to subscription list

--- admin/jqadm/src/Admin/JQAdm/Subscription/Standard.php
+++ admin/jqadm/src/Admin/JQAdm/Subscription/Standard.php
@@ -340,7 +340,7 @@ class Standard
 			$manager = \Aimeos\MShop\Factory::createManager( $context, 'subscription' );
 
 			$search = $manager->createSearch();
-			$search->setSortations( [$search->sort( '-', 'subscription.id' )] );
+			$search->setSortations( [$search->sort( '-', 'subscription.ctime' )] );
 			$search = $this->initCriteria( $search, $params );
 
 			$view->items = $manager->searchItems( $search, [], $total );
@@ -498,7 +498,7 @@ class Standard
 		$search->setConditions( $search->compare( '==', 'order.base.id', $baseIds ) );
 		$search->setSlice( 0, 0x7fffffff );
 
-		return $manager->searchItems( $search, ['order/base/address'] );
+		return $manager->searchItems( $search, ['order/base/address', 'order/base/product'] );
 	}
 
 
