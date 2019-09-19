92fdabf60950984db75a33c98301dca1ba414e3b
Aimeos <aimeos@aimeos.org>
2019-09-19 18:48:29 +0200

    Added missing DeepL configuration

--- admin/jqadm/templates/service/item-text-standard.php
+++ admin/jqadm/templates/service/item-text-standard.php
@@ -17,6 +17,7 @@ $keys = [
 ?>
 <div id="text" class="item-text content-block tab-pane fade" role="tablist" aria-labelledby="text">
 	<div id="item-text-group" role="tablist" aria-multiselectable="true"
+		data-translate="<?= $enc->attr( json_encode( $this->config( 'admin/jqadm/api/translate' ) ) ) ?>"
 		data-items="<?= $enc->attr( json_encode( $this->get( 'textData', [] ) ) ); ?>"
 		data-listtype="<?= key( $this->get( 'textListTypes', [] ) ) ?>"
 		data-keys="<?= $enc->attr( json_encode( $keys ) ) ?>"
