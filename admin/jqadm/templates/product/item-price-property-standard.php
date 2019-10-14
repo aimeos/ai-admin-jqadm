a54307f30820fe2d01a47ebc470de77fbe217c46
Aimeos <aimeos@aimeos.org>
2019-10-14 10:20:47 +0200

    Added "Please select" for property types

--- admin/jqadm/templates/product/item-price-property-standard.php
+++ admin/jqadm/templates/product/item-price-property-standard.php
@@ -42,6 +42,10 @@ $enc = $this->encoder();
 						v-bind:readonly="checkSite('price.siteid', idx)"
 						v-model="items[idx]['property'][propidx]['price.property.type']" >
 
+						<option value="" disabled="disabled">
+							<?= $enc->html( $this->translate( 'admin', 'Please select' ) ); ?>
+						</option>
+
 						<?php foreach( $this->get( 'propertyTypes', [] ) as $type => $item ) : ?>
 							<option value="<?= $enc->attr( $type ); ?>" v-bind:selected="items[idx]['property'][propidx]['price.property.type'] == '<?= $enc->attr( $type ) ?>'" >
 								<?= $enc->html( $item->getLabel() ); ?>
