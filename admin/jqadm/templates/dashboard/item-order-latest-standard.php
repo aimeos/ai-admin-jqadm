25725a68106deae2e6867f96ad2e2d925c5a321b
Aimeos <aimeos@aimeos.org>
2020-02-02 15:55:53 +0100

    Copyright update

--- admin/jqadm/templates/dashboard/item-order-latest-standard.php
+++ admin/jqadm/templates/dashboard/item-order-latest-standard.php
@@ -2,7 +2,7 @@
 
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Aimeos (aimeos.org), 2016-2018
+ * @copyright Aimeos (aimeos.org), 2016-2020
  */
 
 $price = function( \Aimeos\Map $orders, \Aimeos\MShop\Order\Item\Iface $item, $priceFormat )
