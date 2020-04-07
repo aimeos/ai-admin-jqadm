61ce924d87476a63e1cf188600c64bbca95eb283
Aimeos <aimeos@aimeos.org>
2020-04-07 16:39:31 +0200

    Allow div, p and span tags with classes in CKEditor

--- admin/jqadm/themes/vue-components.js
+++ admin/jqadm/themes/vue-components.js
@@ -132,6 +132,7 @@ Vue.component('html-editor', {
 
 	mounted: function() {
 		this.instance = CKEDITOR.replace(this.id, {
+			extraAllowedContent: 'div(*);span(*);p(*);',
 			toolbar: Aimeos.editorcfg,
 			extraPlugins: 'divarea',
 			initialData: this.value,
