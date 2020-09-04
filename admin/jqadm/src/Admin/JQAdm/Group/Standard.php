f0ad8daa9ce8699f04212cc2c92e6bf210b670ae
Aimeos <aimeos@aimeos.org>
2020-09-04 14:03:14 +0200

    Use parent methods for calling subparts in group panel

--- admin/jqadm/src/Admin/JQAdm/Group/Standard.php
+++ admin/jqadm/src/Admin/JQAdm/Group/Standard.php
@@ -55,13 +55,7 @@ class Standard
 			$view->item = $manager->getItem( $id );
 
 			$view->itemData = $this->toArray( $view->item, true );
-			$view->itemBody = '';
-
-			foreach( $this->getSubClients() as $idx => $client )
-			{
-				$view->tabindex = ++$idx + 1;
-				$view->itemBody .= $client->copy();
-			}
+			$view->itemBody = parent::copy();
 		}
 		catch( \Exception $e )
 		{
@@ -92,13 +86,7 @@ class Standard
 			$data['customer.group.siteid'] = $view->item->getSiteId();
 
 			$view->itemData = array_replace_recursive( $this->toArray( $view->item ), $data );
-			$view->itemBody = '';
-
-			foreach( $this->getSubClients() as $idx => $client )
-			{
-				$view->tabindex = ++$idx + 1;
-				$view->itemBody .= $client->create();
-			}
+			$view->itemBody = parent::create();
 		}
 		catch( \Exception $e )
 		{
@@ -134,10 +122,7 @@ class Standard
 			foreach( $items as $item )
 			{
 				$view->item = $item;
-
-				foreach( $this->getSubClients() as $client ) {
-					$client->delete();
-				}
+				parent::delete();
 			}
 
 			$manager->deleteItems( $items->toArray() );
@@ -174,13 +159,7 @@ class Standard
 
 			$view->item = $manager->getItem( $id );
 			$view->itemData = $this->toArray( $view->item );
-			$view->itemBody = '';
-
-			foreach( $this->getSubClients() as $idx => $client )
-			{
-				$view->tabindex = ++$idx + 1;
-				$view->itemBody .= $client->get();
-			}
+			$view->itemBody = parent::get();
 		}
 		catch( \Exception $e )
 		{
@@ -207,11 +186,7 @@ class Standard
 		{
 			$item = $this->fromArray( $view->param( 'item', [] ) );
 			$view->item = $item->getId() ? $item : $manager->saveItem( $item );
-			$view->itemBody = '';
-
-			foreach( $this->getSubClients() as $client ) {
-				$view->itemBody .= $client->save();
-			}
+			$view->itemBody = parent::save();
 
 			$manager->saveItem( clone $view->item );
 			$manager->commit();
@@ -247,12 +222,8 @@ class Standard
 			$view->items = $manager->searchItems( $search, [], $total );
 			$view->filterAttributes = $manager->getSearchAttributes( true );
 			$view->filterOperators = $search->getOperators();
+			$view->itemBody = parent::search();
 			$view->total = $total;
-			$view->itemBody = '';
-
-			foreach( $this->getSubClients() as $client ) {
-				$view->itemBody .= $client->search();
-			}
 		}
 		catch( \Exception $e )
 		{
