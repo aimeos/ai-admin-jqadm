65f30cfb5a1e1acad52ad61e2ba5425d20784412
Aimeos <aimeos@aimeos.org>
2020-09-10 16:59:48 +0200

    Disallow site admins to access the locale/site panel

--- config/admin/jqadm/resource.php
+++ config/admin/jqadm/resource.php
@@ -229,7 +229,7 @@ return [
 			 * @param array List of user group names
 			 * @since 2017.10
 			 */
-			'groups' => ['admin', 'super'],
+			'groups' => ['super'],
 		],
 		'language' => [
 			/** admin/jqadm/resource/locale/language/groups
