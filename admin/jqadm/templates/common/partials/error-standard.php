51bcd10a35e024fa6453d42fcd67b461255e3ae0
Aimeos <aimeos@aimeos.org>
2021-02-21 11:28:38 +0100

    Minimized HTML in error/info partials if no messages are available

--- admin/jqadm/templates/common/partials/error-standard.php
+++ admin/jqadm/templates/common/partials/error-standard.php
@@ -29,30 +29,33 @@
  */
 
 $enc = $this->encoder();
+$list = $this->get( 'errors', [] );
 
 
 ?>
-<div class="toast-list" aria-live="polite" aria-atomic="true">
+<?php if( !empty( $list ) ) : ?>
+	<div class="toast-list" aria-live="polite" aria-atomic="true">
 
-<?php foreach( $this->get( 'errors', [] ) as $key => $entry ) : ?>
-	<div class="row error toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-bs-autohide="false">
-		<div class="col-1 toast-icon">
-			<span class="fa fa-exclamation-circle"></span>
-		</div>
-		<div class="col-9">
-			<div class="toast-header">
-				<strong><?= $enc->html( $this->translate( 'admin', 'Error' ) ); ?></strong>
+	<?php foreach( $list as $key => $entry ) : ?>
+		<div class="row error toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-bs-autohide="false">
+			<div class="col-1 toast-icon">
+				<span class="fa fa-exclamation-circle"></span>
 			</div>
-			<div class="toast-body">
-				<?= $enc->html( $entry ); ?>
+			<div class="col-9">
+				<div class="toast-header">
+					<strong><?= $enc->html( $this->translate( 'admin', 'Error' ) ); ?></strong>
+				</div>
+				<div class="toast-body">
+					<?= $enc->html( $entry ); ?>
+				</div>
+			</div>
+			<div class="col-2 toast-close">
+				<button type="button" class="btn-close" data-bs-dismiss="toast"
+					aria-label="<?= $enc->attr( $this->translate( 'admin', 'Close' ) ) ?>">
+				</button>
 			</div>
 		</div>
-		<div class="col-2 toast-close">
-			<button type="button" class="btn-close" data-bs-dismiss="toast"
-				aria-label="<?= $enc->attr( $this->translate( 'admin', 'Close' ) ) ?>">
-			</button>
-		</div>
+	<?php endforeach; ?>
+
 	</div>
-<?php endforeach; ?>
-
-</div>
+<?php endif ?>
--- admin/jqadm/templates/common/partials/info-standard.php
+++ admin/jqadm/templates/common/partials/info-standard.php
@@ -29,30 +29,33 @@
  */
 
 $enc = $this->encoder();
+$list = $this->get( 'info', [] );
 
 
 ?>
-<div class="toast-list" aria-live="polite" aria-atomic="true">
+<?php if( !empty( $list ) ) : ?>
+	<div class="toast-list" aria-live="polite" aria-atomic="true">
 
-	<?php foreach( $this->get( 'info', [] ) as $key => $entry ) : ?>
-		<div class="row info toast hide" role="alert" aria-live="assertive" aria-atomic="true">
-			<div class="col-1 toast-icon">
-				<span class="fa fa-exclamation-circle"></span>
-			</div>
-			<div class="col-9">
-				<div class="toast-header">
-					<strong><?= $enc->html( $this->translate( 'admin', 'Info' ) ); ?></strong>
+		<?php foreach( $list as $key => $entry ) : ?>
+			<div class="row info toast hide" role="alert" aria-live="assertive" aria-atomic="true">
+				<div class="col-1 toast-icon">
+					<span class="fa fa-exclamation-circle"></span>
 				</div>
-				<div class="toast-body">
-					<?= $enc->html( $entry ); ?>
+				<div class="col-9">
+					<div class="toast-header">
+						<strong><?= $enc->html( $this->translate( 'admin', 'Info' ) ); ?></strong>
+					</div>
+					<div class="toast-body">
+						<?= $enc->html( $entry ); ?>
+					</div>
+				</div>
+				<div class="col-2 toast-close">
+					<button type="button" class="btn-close" data-bs-dismiss="toast"
+						aria-label="<?= $enc->attr( $this->translate( 'admin', 'Close' ) ) ?>">
+					</button>
 				</div>
 			</div>
-			<div class="col-2 toast-close">
-				<button type="button" class="btn-close" data-bs-dismiss="toast"
-					aria-label="<?= $enc->attr( $this->translate( 'admin', 'Close' ) ) ?>">
-				</button>
-			</div>
-		</div>
-	<?php endforeach; ?>
+		<?php endforeach; ?>
 
-</div>
+	</div>
+<?php endif ?>
