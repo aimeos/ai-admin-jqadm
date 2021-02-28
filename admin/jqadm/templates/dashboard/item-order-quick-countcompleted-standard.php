fa6a8b8d9a7f782f8dae00532f1857bf3bbdbb20
Aimeos <aimeos@aimeos.org>
2021-02-28 15:25:23 +0100

    Implemented quick completed order count as Vue component

--- admin/jqadm/templates/dashboard/item-order-quick-countcompleted-standard.php
+++ admin/jqadm/templates/dashboard/item-order-quick-countcompleted-standard.php
@@ -11,17 +11,19 @@ $enc = $this->encoder();
 ?>
 <div class="quick order-quick-countcompleted col-sm-6 col-xl-3"
 	title="<?= $enc->attr( $this->translate( 'admin', 'Completed orders within the last seven days compared to the period before' ) ) ?>">
-	<div class="box row">
-		<div class="col quick-left">
-			<div class="quick-number">0</div>
+	<dashboard-order-quick-countcompleted inline-template>
+		<div class="box row" v-bind:class="state">
+			<div class="col quick-left">
+				<div class="quick-number" v-html="current">0</div>
+			</div>
+			<div class="col quick-right">
+				<div class="quick-percent" v-bind:class="mood" v-html="percent"></div>
+			</div>
+			<div class="col-xs-12">
+				<h2 class="quick-header"><?= $enc->html( $this->translate( 'admin', 'Completed orders' ) ) ?></h2>
+				<div class="quick-progress"><div class="quick-length" v-bind:style="'width: ' + width +'%'"></div></div>
+			</div>
 		</div>
-		<div class="col quick-right">
-			<div class="quick-percent"></div>
-		</div>
-		<div class="col-xs-12">
-			<h2 class="quick-header"><?= $enc->html( $this->translate( 'admin', 'Completed orders' ) ) ?></h2>
-			<div class="quick-progress"><div class="quick-length"></div></div>
-		</div>
-	</div>
+	</dashboard-order-quick-countcompleted>
 </div>
 <?= $this->get( 'quickCountcompletedBody' ); ?>
