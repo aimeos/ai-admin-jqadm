6f7e148dbb6f4a6063dad37b787619e5de6154ba
Aimeos <aimeos@aimeos.org>
2021-04-01 17:27:55 +0200

    Fixed selecting decorators in coupon/service panel

--- admin/jqadm/templates/service/item-standard.php
+++ admin/jqadm/templates/service/item-standard.php
@@ -95,7 +95,7 @@ $enc = $this->encoder();
 
 				<div class="box">
 					<div class="row">
-						<div class="col-xl-6 vue <?= $this->site()->readonly( $this->get( 'itemData/service.siteid' ) ); ?>">
+						<div class="col-xl-6 <?= $this->site()->readonly( $this->get( 'itemData/service.siteid' ) ); ?>">
 							<div class="form-group row mandatory">
 								<label class="col-sm-4 form-control-label"><?= $enc->html( $this->translate( 'admin', 'Status' ) ); ?></label>
 								<div class="col-sm-8">
