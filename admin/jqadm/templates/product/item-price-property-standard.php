c42fd18560db6049b7b89c6f691e454ac6a16f4d
Aimeos <aimeos@aimeos.org>
2021-04-21 09:44:37 +0200

    Fixed adding media/price properties

--- admin/jqadm/templates/product/item-price-property-standard.php
+++ admin/jqadm/templates/product/item-price-property-standard.php
@@ -7,28 +7,32 @@
 
 
 $enc = $this->encoder();
-
+$types = $this->get( 'propertyTypes', map() )->col( 'price.property.type.label', 'price.property.type.code' )->toArray();
 
 ?>
 <div v-show="item['_ext']" class="col-xl-12 secondary">
 
-	<property-table v-if="item['property'] && item['property'].length"
-		v-bind:index="idx" v-bind:domain="'price'"
-		v-bind:siteid="`<?= $enc->js( $this->site()->siteid() ) ?>`" v-bind:tabindex="`<?= $enc->js( $this->get( 'tabindex' ) ) ?>`"
-		v-bind:types="<?= $enc->attr( $this->get( 'propertyTypes', map() )->col( 'price.property.type.label', 'price.property.type.code' )->toArray() ) ?>"
-		v-bind:languages="<?= $enc->attr( $this->get( 'pageLangItems', map() )->col( 'locale.language.label', 'locale.language.id' )->toArray() ) ?>"
-		v-bind:name="`<?= $enc->js( $this->formparam( ['price', '_idx_', 'property', '_propidx_', '_key_'] ) ) ?>`"
-		v-bind:items="item['property']" v-on:update:property="item['property'] = $event"
-		v-bind:i18n="{
-			all: `<?= $enc->js( $this->translate( 'admin', 'All' ) ) ?>`,
-			delete: `<?= $enc->js( $this->translate( 'admin', 'Delete this entry' ) ) ?>`,
-			header: `<?= $enc->js( $this->translate( 'admin', 'Price properties' ) ) ?>`,
-			help: `<?= $enc->js( $this->translate( 'admin', 'Non-shared properties for the price item' ) ) ?>`,
-			insert: `<?= $enc->js( $this->translate( 'admin', 'Insert new entry (Ctrl+I)' ) ) ?>`,
-			placeholder: `<?= $enc->js( $this->translate( 'admin', 'Property value (required)' ) ) ?>`,
-			select: `<?= $enc->js( $this->translate( 'admin', 'Please select' ) ) ?>`
-		}">
-	</property-table>
+	<?php if( !empty( $types ) ) : ?>
+
+		<property-table
+			v-bind:index="idx" v-bind:domain="'price'"
+			v-bind:types="<?= $enc->attr( $types ) ?>"
+			v-bind:siteid="`<?= $enc->js( $this->site()->siteid() ) ?>`" v-bind:tabindex="`<?= $enc->js( $this->get( 'tabindex' ) ) ?>`"
+			v-bind:languages="<?= $enc->attr( $this->get( 'pageLangItems', map() )->col( 'locale.language.label', 'locale.language.id' )->toArray() ) ?>"
+			v-bind:name="`<?= $enc->js( $this->formparam( ['price', '_idx_', 'property', '_propidx_', '_key_'] ) ) ?>`"
+			v-bind:items="item['property']" v-on:update:property="item['property'] = $event"
+			v-bind:i18n="{
+				all: `<?= $enc->js( $this->translate( 'admin', 'All' ) ) ?>`,
+				delete: `<?= $enc->js( $this->translate( 'admin', 'Delete this entry' ) ) ?>`,
+				header: `<?= $enc->js( $this->translate( 'admin', 'Price properties' ) ) ?>`,
+				help: `<?= $enc->js( $this->translate( 'admin', 'Non-shared properties for the price item' ) ) ?>`,
+				insert: `<?= $enc->js( $this->translate( 'admin', 'Insert new entry (Ctrl+I)' ) ) ?>`,
+				placeholder: `<?= $enc->js( $this->translate( 'admin', 'Property value (required)' ) ) ?>`,
+				select: `<?= $enc->js( $this->translate( 'admin', 'Please select' ) ) ?>`
+			}">
+		</property-table>
+
+	<?php endif ?>
 
 	<?= $this->get( 'propertyBody' ) ?>
 
