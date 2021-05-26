1c29577462ea3ce07524284156c6d74d0d94ea5b
Aimeos <aimeos@aimeos.org>
2021-05-26 08:12:59 +0200

    Minor layout improvements

--- admin/jqadm/themes/vue-components.js
+++ admin/jqadm/themes/vue-components.js
@@ -1,6 +1,6 @@
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Aimeos (aimeos.org), 2017-2018
+ * @copyright Aimeos (aimeos.org), 2017-2021
  */
 
 
@@ -652,7 +652,7 @@ Vue.component('taxrates', {
 							v-bind:value="val" v-on:input="update(type, $event.target.value)" /> \
 						<div v-if="type != 0" class="input-group-append"><span class="input-group-text">{{ type.toUpperCase() }}</span></div> \
 					</td> \
-					<td class="actions"> \
+					<td v-if="!readonly && types.length" class="actions"> \
 						<div v-if="!readonly && type == 0 && types.length" class="dropdown"> \
 							<button class="btn act-add fa" v-bind:tabindex="tabindex" \
 								type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> \
