92bdf959521abf9fac380ca54414c3d68544da87
Aimeos <aimeos@aimeos.org>
2021-06-20 15:11:15 +0200

    Only allow super users to change passwords

--- admin/jqadm/src/Admin/JQAdm/Customer/Standard.php
+++ admin/jqadm/src/Admin/JQAdm/Customer/Standard.php
@@ -476,10 +476,19 @@ class Standard
 		$label .= ( $addr->getCompany() ? ' (' . $addr->getCompany() . ')' : '' );
 		$groupIds = $this->getValue( $data, 'customer.groups', [] );
 
-		return $item->fromArray( $data, true )
+		$pass = $data['customer.password'] ?? null;
+		unset( $data['customer.password'] );
+
+		$item->fromArray( $data, true )
 			->setGroups( array_intersect( array_keys( $this->getGroupItems( $item ) ), $groupIds ) )
 			->setCode( $item->getCode() ?: $addr->getEmail() )
 			->setLabel( $label );
+
+		if( $this->getView()->access( ['super'] ) ) {
+			$item->setPassword( $pass );
+		}
+
+		return $item;
 	}
 
 
@@ -492,6 +501,10 @@ class Standard
 	protected function toArray( \Aimeos\MShop\Customer\Item\Iface $item, bool $copy = false ) : array
 	{
 		$data = $item->toArray( true );
+
+		if( !$this->getView()->access( ['super'] ) ) {
+			unset( $data['customer.password'] );
+		}
 
 		if( $copy === true )
 		{
