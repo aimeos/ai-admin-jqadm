ac3ee33d683fa124009f954ef0eb7d6b0ab13ff8
Aimeos <aimeos@aimeos.org>
2021-10-27 08:46:08 +0200

    Revert "Fixed showing children by default"
    This reverts commit 34ad957590f53aa0b75c92ddaa730dfd498b78d8.

--- admin/jqadm/js/vue-components.js
+++ admin/jqadm/js/vue-components.js
@@ -746,7 +746,7 @@ Vue.component('site-tree-items', {
 						'icon-loading fa-pulse': item.isLoading
 					}">
 				</span>
-				<site-tree-items v-if="isAvailable(item) && ( item.isOpen || typeof item.isOpen === 'undefined' )"
+				<site-tree-items v-if="isAvailable(item) && item.isOpen"
 					v-on="$listeners"
 					v-on:loading="loading(id, $event)"
 					v-bind:initial="item.children || {}"
@@ -887,7 +887,7 @@ Vue.component('site-tree-items', {
 		},
 
 		toggle(id) {
-			this.$set(this.items[id], 'isOpen', typeof this.items[id].isOpen !== 'undefined' ? !this.items[id].isOpen : false);
+			this.$set(this.items[id], 'isOpen', !this.items[id].isOpen);
 		}
 	},
 
