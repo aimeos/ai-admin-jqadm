2b07d3a7a103920de26a5e4521a99ac638dafa17
Aimeos <aimeos@aimeos.org>
2022-05-19 14:21:57 +0200

    Added theme settings sub-panel

---
+++ src/Admin/JQAdm/Settings/Theme/Standard.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * @copyright Aimeos GmbH (aimeos.com), 2022
+ * @package Admin
+ * @subpackage JQAdm
+ */
+
+
+namespace Aimeos\Admin\JQAdm\Settings\Theme;
+
+sprintf( 'theme' ); // for translation
+
+
+/**
+ * Default implementation of settings theme JQAdm client.
+ *
+ * @package Admin
+ * @subpackage JQAdm
+ */
+class Standard
+	extends \Aimeos\Admin\JQAdm\Common\Admin\Factory\Base
+	implements \Aimeos\Admin\JQAdm\Common\Admin\Factory\Iface
+{
+	/**
+	 * Saves the data
+	 *
+	 * @return string|null HTML output
+	 */
+	public function save() : ?string
+	{
+		$context = $this->context();
+		$site = $context->locale()->getSiteItem();
+		$theme = $site->getTheme() ?: 'default';
+
+		$site->setConfigValue( 'theme', $this->view()->param( 'theme/' . $theme, [] ) );
+		return null;
+	}
+
+
+	/**
+	 * Returns the resource
+	 *
+	 * @return string|null HTML output
+	 */
+	public function search() : ?string
+	{
+		$context = $this->context();
+		$site = $context->locale()->getSiteItem();
+		$theme = $site->getTheme() ?: 'default';
+
+		$themeData = $site->getConfigValue( 'theme/' . $theme, [] );
+		$themeData = array_replace_recursive( $context->config()->get( 'client/html/theme-presets/' . $theme ), $themeData );
+
+		$view = $this->object()->data( $this->view() );
+		$view->themeData = $themeData;
+
+		return $this->render( $view );
+	}
+
+
+	/**
+	 * Returns the sub-client given by its name.
+	 *
+	 * @param string $type Name of the client type
+	 * @param string|null $name Name of the sub-client (Default if null)
+	 * @return \Aimeos\Admin\JQAdm\Iface Sub-client object
+	 */
+	public function getSubClient( string $type, string $name = null ) : \Aimeos\Admin\JQAdm\Iface
+	{
+		return $this->createSubClient( 'settings/theme/' . $type, $name );
+	}
+
+
+	/**
+	 * Returns the list of sub-client names configured for the client.
+	 *
+	 * @return array List of JQAdm client names
+	 */
+	protected function getSubClientNames() : array
+	{
+		return $this->context()->config()->get( 'admin/jqadm/settings/theme/subparts', [] );
+	}
+
+
+	/**
+	 * Returns the rendered template including the view data
+	 *
+	 * @param \Aimeos\Base\View\Iface $view View object with data assigned
+	 * @return string HTML output
+	 */
+	protected function render( \Aimeos\Base\View\Iface $view ) : string
+	{
+		$tplconf = 'admin/jqadm/settings/theme/template-item';
+		$default = 'settings/item-theme';
+
+		return $view->render( $view->config( $tplconf, $default ) );
+	}
+}
---
+++ templates/admin/jqadm/settings/item-theme.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2021-2022
+ */
+
+$selected = function( $key, $code ) {
+	return ( $key == $code ? 'selected="selected"' : '' );
+};
+
+$enc = $this->encoder();
+
+
+?>
+<div id="theme" class="item-theme tab-pane fade" role="tabpanel" aria-labelledby="theme">
+
+	<div class="box">
+		<div class="row">
+			<div class="col-lg-6">
+
+				<?php foreach( $this->get( 'themeData', [] ) as $key => $value ) : ?>
+					
+					<div class="form-group row mandatory">
+						<label class="col-sm-4 form-control-label"><?= $enc->html( $key ) ?></label>
+						<div class="col-sm-8">
+							<input class="form-control" type="<?= strncmp( $value, '#', 1 ) ? 'color' : 'text' ?>"
+								required="required" tabindex="<?= $this->get( 'tabindex' ) ?>"
+								name="<?= $enc->attr( $this->formparam( ['theme', $key] ) ) ?>"
+								value="<?= $enc->attr( $value ) ?>"
+							/>
+						</div>
+					</div>
+
+				<?php endforeach ?>
+
+			</div>
+			<div class="col-lg-6"></div>
+		</div>
+	</div>
+
+</div>
