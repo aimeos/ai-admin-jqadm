a33bf6df84c77f41cce8ebecd8b65000e7a436db
Aimeos <aimeos@aimeos.org>
2022-06-20 17:06:46 +0200

    Implemented batch operations for locale list panel

--- src/Admin/JQAdm/Locale/Standard.php
+++ src/Admin/JQAdm/Locale/Standard.php
@@ -86,6 +86,17 @@ class Standard
 
 
 	/**
+	 * Batch update of a resource
+	 *
+	 * @return string|null Output to display
+	 */
+	public function batch() : ?string
+	{
+		return $this->batchBase( 'locale' );
+	}
+
+
+	/**
 	 * Copies a resource
 	 *
 	 * @return string|null HTML output
