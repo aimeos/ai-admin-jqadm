1f874982364e85523380466bf76ad4d88d8a360e
Aimeos <aimeos@aimeos.org>
2022-06-27 09:07:19 +0200

    Implemented batch operations in service type list views

--- tests/Admin/JQAdm/Type/Service/Lists/StandardTest.php
+++ tests/Admin/JQAdm/Type/Service/Lists/StandardTest.php
@@ -34,6 +34,28 @@ class StandardTest extends \PHPUnit\Framework\TestCase
 	}
 
 
+	public function testBatch()
+	{
+		$param = array(
+			'site' => 'unittest',
+			'item' => array(
+				'service.lists.type.domain' => 'product',
+				'service.lists.type.position' => 1,
+				'service.lists.type.status' => -1,
+			),
+			'id' => [-1, -2]
+		);
+
+		$helper = new \Aimeos\Base\View\Helper\Param\Standard( $this->view, $param );
+		$this->view->addHelper( 'param', $helper );
+
+		$result = $this->object->batch();
+
+		$this->assertEmpty( $this->view->get( 'errors' ) );
+		$this->assertNull( $result );
+	}
+
+
 	public function testCreate()
 	{
 		$result = $this->object->create();
