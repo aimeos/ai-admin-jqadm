7f99c64938f170ae179ecb693f9037836b9eb99d
Aimeos <aimeos@aimeos.org>
2022-07-04 11:32:01 +0200

    Added disabled attribute to taxrate component

--- js/components/taxrates.js
+++ js/components/taxrates.js
@@ -11,12 +11,12 @@ Vue.component('taxrates', {
 				<tr v-for="(val, type) in taxrates" v-bind:key="type"> \
 					<td class="input-group"> \
 						<input class="form-control item-taxrate" required="required" step="0.01" type="number" v-bind:placeholder="placeholder" \
-							v-bind:readonly="readonly" v-bind:tabindex="tabindex" v-bind:name="name + \'[\' + type + \']\'" \
+							v-bind:disabled="disabled" v-bind:readonly="readonly" v-bind:tabindex="tabindex" v-bind:name="name + \'[\' + type + \']\'" \
 							v-bind:value="val" v-on:input="update(type, $event.target.value)" /> \
 						<div v-if="type != 0" class="input-group-append"><span class="input-group-text">{{ type.toUpperCase() }}</span></div> \
 					</td> \
-					<td v-if="!readonly && types.length" class="actions"> \
-						<div v-if="!readonly && type == 0 && types.length" class="dropdown"> \
+					<td v-if="(!disabled && !readonly) && types.length" class="actions"> \
+						<div v-if="(!disabled && !readonly) && type == 0 && types.length" class="dropdown"> \
 							<button class="btn act-add fa" v-bind:tabindex="tabindex" \
 								type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> \
 							</button> \
@@ -24,13 +24,13 @@ Vue.component('taxrates', {
 								<a v-for="(rate, code) in types" class="dropdown-item" href="#" v-on:click="add(code, rate)">{{ code.toUpperCase() }}</a> \
 							</div> \
 						</div> \
-						<div v-if="!readonly && type != 0" class="btn act-delete fa" v-on:click="remove(type)"></div> \
+						<div v-if="(!disabled && !readonly) && type != 0" class="btn act-delete fa" v-on:click="remove(type)"></div> \
 					</td> \
 				</tr> \
 			</table> \
 		</div> \
 	',
-	props: ['name', 'placeholder', 'readonly', 'tabindex', 'taxrates', 'types'],
+	props: ['name', 'placeholder', 'disabled', 'readonly', 'tabindex', 'taxrates', 'types'],
 
 	created: function() {
 		delete this.types[''];
