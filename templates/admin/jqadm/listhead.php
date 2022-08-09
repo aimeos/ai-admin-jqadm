5fadb1f69e2a3a6c31806fbcb83f52ebfbe114b3
Aimeos <aimeos@aimeos.org>
2022-08-09 18:11:27 +0200

    Fixed sorting in list views

--- templates/admin/jqadm/listhead.php
+++ templates/admin/jqadm/listhead.php
@@ -65,7 +65,7 @@ else
 <?php foreach( $this->get( 'data', [] ) as $key => $name ) : ?>
 	<?php if( in_array( $key, $fields ) ) : ?>
 		<th class="<?= $enc->attr( str_replace( '.', '-', $key ) ) ?>">
-			<?php if( strpos( $name, '.' ) !== false ) : ?>
+			<?php if( strpos( $key, '.' ) !== false ) : ?>
 				<a class="<?= $sortclass( $sortcode, $key ) ?>" tabindex="<?= $this->get( 'tabindex', 1 ) ?>"
 					href="<?= $enc->attr( $this->link( $cfgkey, $nest( $group, ['sort' => $sort( $sortcode, $key )] ) + $params, [], $fragment ) ) ?>">
 					<?= $enc->html( $name ) ?>
