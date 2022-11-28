f2be16203272a87bb220a017a23d3ed55bae587b
Aimeos <aimeos@aimeos.org>
2022-11-28 15:40:43 +0100

    Fixed and improved order transaction template

--- templates/admin/jqadm/order/item-transaction.php
+++ templates/admin/jqadm/order/item-transaction.php
@@ -23,41 +23,41 @@ $paymentStatusList = [
 
 
 ?>
-<div id="transaction" class="item-transaction tab-pane fade box" role="tabpanel" aria-labelledby="transaction">
-
-	<h2><?= $enc->html( $this->translate( 'admin', 'Refunds' ) ) ?></h2>
+<div id="transaction" class="item-transaction tab-pane fade" role="tabpanel" aria-labelledby="transaction">
 
 	<?php foreach( $this->item->getService( 'payment') as $service ) : ?>
 
-		<h3><?= $enc->html( $service->getName() ) ?></h3>
+		<div class="box">
+			<h2 class="item-header"><?= $enc->html( $service->getName() ) ?></h2>
 
-		<div class="table-responsive">
-			<table class="list-items table table-striped">
-				<thead class="list-header">
-					<tr>
-						<th class="transaction-id"><?= $enc->html( $this->translate( 'admin', 'ID' ) ) ?></th>
-						<th class="transaction-type"><?= $enc->html( $this->translate( 'admin', 'Type' ) ) ?></th>
-						<th class="transaction-currencyid"><?= $enc->html( $this->translate( 'admin', 'Currency' ) ) ?></th>
-						<th class="transaction-price"><?= $enc->html( $this->translate( 'admin', 'Price' ) ) ?></th>
-						<th class="transaction-costs"><?= $enc->html( $this->translate( 'admin', 'Costs' ) ) ?></th>
-						<th class="transaction-status"><?= $enc->html( $this->translate( 'admin', 'Status' ) ) ?></th>
-						<th class="transaction-data"><?= $enc->html( $this->translate( 'admin', 'Data' ) ) ?></th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php foreach( $service->getTransactions() as $txItem ) : ?>
+			<div class="table-responsive">
+				<table class="list-items table table-striped">
+					<thead class="list-header">
 						<tr>
-							<td class="transaction-id"><?= $enc->html( $txItem->getId() ) ?></td>
-							<td class="transaction-type"><?= $enc->html( $txItem->getType() ) ?></td>
-							<td class="transaction-currencyid"><?= $enc->html( $txItem->getPrice()->getCurrencyId() ) ?></td>
-							<td class="transaction-price"><?= $enc->html( $txItem->getPrice()->getValue() ) ?></td>
-							<td class="transaction-costs"><?= $enc->html( $txItem->getPrice()->getCosts() ) ?></td>
-							<td class="transaction-status"><?= $enc->html( $paymentStatusList[$txItem->getStatus()] ?? $txItem->getStatus() ) ?></td>
-							<td class="transaction-data"><?= str_replace( ',', '<br>', $enc->html( trim( json_encode( $txItem->getData() ) ), '{}' ) ) ?></td>
+							<th class="transaction-id"><?= $enc->html( $this->translate( 'admin', 'ID' ) ) ?></th>
+							<th class="transaction-type"><?= $enc->html( $this->translate( 'admin', 'Type' ) ) ?></th>
+							<th class="transaction-currencyid"><?= $enc->html( $this->translate( 'admin', 'Currency' ) ) ?></th>
+							<th class="transaction-price"><?= $enc->html( $this->translate( 'admin', 'Price' ) ) ?></th>
+							<th class="transaction-costs"><?= $enc->html( $this->translate( 'admin', 'Costs' ) ) ?></th>
+							<th class="transaction-status"><?= $enc->html( $this->translate( 'admin', 'Status' ) ) ?></th>
+							<th class="transaction-data"><?= $enc->html( $this->translate( 'admin', 'Data' ) ) ?></th>
 						</tr>
-					<?php endforeach ?>
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						<?php foreach( $service->getTransactions() as $txItem ) : ?>
+							<tr>
+								<td class="transaction-id"><?= $enc->html( $txItem->getId() ) ?></td>
+								<td class="transaction-type"><?= $enc->html( $txItem->getType() ) ?></td>
+								<td class="transaction-currencyid"><?= $enc->html( $txItem->getPrice()->getCurrencyId() ) ?></td>
+								<td class="transaction-price"><?= $enc->html( $txItem->getPrice()->getValue() ) ?></td>
+								<td class="transaction-costs"><?= $enc->html( $txItem->getPrice()->getCosts() ) ?></td>
+								<td class="transaction-status"><?= $enc->html( $paymentStatusList[$txItem->getStatus()] ?? $txItem->getStatus() ) ?></td>
+								<td class="transaction-data"><?= str_replace( [',', '{', '}'], '<br>', $enc->html( str_replace( [':', '"'], [': ', ''], trim( json_encode( $txItem->getConfig() ), '{}' ) ) ) ) ?></td>
+							</tr>
+						<?php endforeach ?>
+					</tbody>
+				</table>
+			</div>
 		</div>
 
 	<?php endforeach ?>
