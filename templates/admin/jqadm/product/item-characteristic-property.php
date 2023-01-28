5fe0b98cb3661ce377959cbd481ea2c2d574007e
maynor96 <mayeral96@gmail.com>
2023-01-28 13:08:26 -0600

    add object cast to property types

--- templates/admin/jqadm/product/item-characteristic-property.php
+++ templates/admin/jqadm/product/item-characteristic-property.php
@@ -8,13 +8,6 @@
 
 $enc = $this->encoder();
 
-$propertyTypes = $this->get( 'propertyTypes', map());
-
-if ( $propertyTypes->isEmpty() ) {
-    $propertyTypes = (object)[];
-} else {
-    $propertyTypes = $propertyTypes->col( 'product.property.type.label', 'product.property.type.code' )->all();
-}
 
 ?>
 <div class="col-xl-12 vue" data-key="characteristic/property"
@@ -23,7 +16,7 @@ if ( $propertyTypes->isEmpty() ) {
 	<div class="box">
 		<property-table
 			v-bind:domain="'product'" v-bind:siteid="`<?= $enc->js( $this->site()->siteid() ) ?>`" v-bind:tabindex="`<?= $enc->js( $this->get( 'tabindex' ) ) ?>`"
-            v-bind:types="<?= $enc->attr( $propertyTypes ) ?>"
+			v-bind:types="<?= $enc->attr( (object) $this->get( 'propertyTypes', map() )->col( 'product.property.type.label', 'product.property.type.code' )->all() ) ?>"
 			v-bind:languages="<?= $enc->attr( $this->get( 'pageLangItems', map() )->col( 'locale.language.label', 'locale.language.id' )->unshift( $this->translate( 'admin', '__hidden__' ), 'xx' )->all() ) ?>"
 			v-bind:name="`<?= $enc->js( $this->formparam( ['characteristic', 'property', '_propidx_', '_key_'] ) ) ?>`"
 			v-bind:items="data" v-on:update:property="data = $event"
