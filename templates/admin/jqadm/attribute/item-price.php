ee5b89c3be19d9f8af3a3dc3ec7ad14a3408a008
Aimeos <aimeos@aimeos.org>
2023-04-14 10:02:38 +0200

    Fixed parameter order for can() in templates

--- templates/admin/jqadm/attribute/item-price.php
+++ templates/admin/jqadm/attribute/item-price.php
@@ -43,11 +43,11 @@ $enc = $this->encoder();
 						</div>
 						<span class="item-label header-label" v-bind:class="{disabled: !active(idx)}">{{ label(idx) }}</span>
 						<div class="card-tools-end">
-							<div v-if="can(idx, 'move')"
+							<div v-if="can('move', idx)"
 								class="btn btn-card-header act-move fa" tabindex="<?= $this->get( 'tabindex' ) ?>"
 								title="<?= $enc->attr( $this->translate( 'admin', 'Move this entry up/down' ) ) ?>">
 							</div>
-							<div v-if="can(idx, 'delete')"
+							<div v-if="can('delete', idx)"
 								class="btn btn-card-header act-delete fa" tabindex="<?= $this->get( 'tabindex' ) ?>"
 								title="<?= $enc->attr( $this->translate( 'admin', 'Delete this entry' ) ) ?>"
 								v-on:click.stop="remove(idx)">
