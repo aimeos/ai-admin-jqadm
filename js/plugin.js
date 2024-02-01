5e22c966dd68f76b56f21bb7685edfee4fd5245e
Aimeos <aimeos@aimeos.org>
2024-02-01 15:01:29 +0100

    Migrated plugin panel to VueJS 3.x

--- js/plugin.js
+++ js/plugin.js
@@ -8,28 +8,29 @@
 Aimeos.Plugin = {
 
 	init() {
-		const { createApp } = Vue
+		const node = document.querySelector('.item-plugin #basic');
 
-		Aimeos.apps['plugin'] = createApp({
-			el: document.querySelector('.item-plugin #basic'),
-			data() {
-				return {
-					item: null,
-					cache: {},
-					decorators: [],
-					providers: [],
-					siteid: null,
-				}
-			},
-			beforeMount() {
-				this.Aimeos = Aimeos;
-				this.decorators = JSON.parse(this.$el.dataset.decorators || '[]');
-				this.providers = JSON.parse(this.$el.dataset.providers || '[]');
-				this.item = JSON.parse(this.$el.dataset.item || '{}');
-				this.siteid = this.$el.dataset.siteid;
-			},
-			mixins: [this.mixins]
-		});
+		if(node) {
+			Aimeos.apps['plugin'] = Aimeos.app({
+				props: {
+					data: {type: String, default: '{}'},
+					siteid: {type: String, default: ''},
+					providers: {type: String, default: '[]'},
+					decorators: {type: String, default: '[]'},
+				},
+				data() {
+					return {
+						item: null,
+						cache: {},
+					}
+				},
+				beforeMount() {
+					this.Aimeos = Aimeos;
+					this.item = JSON.parse(this.data);
+				},
+				mixins: [this.mixins]
+			}, {...node.dataset || {}}).mount(node);
+		}
 	},
 
 
