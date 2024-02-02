543cc41daa5ce18909193bf0d1587f71a9fae698
Aimeos <aimeos@aimeos.org>
2024-02-02 09:50:24 +0100

    Updated select component for VueJS 3.x

--- js/components/select-component.js
+++ js/components/select-component.js
@@ -6,20 +6,20 @@
 
 Aimeos.components['select-component'] = {
 	template: `
-		<select v-on:input="$emit('input', $event.target.value)" v-bind:value="value">
+		<select @update:modelValue="$emit('update:modelValue', $event.target.value)" v-bind:value="modelValue">
 			<option v-if="text" value="">{{ text }}</option>
-			<option v-if="value && !items[value]" v-bind:value="value">{{ value }}</option>
-			<option v-if="all" v-bind:value="null" v-bind:selected="value === null">{{ all }}</option>
+			<option v-if="modelValue && !items[modelValue]" v-bind:value="modelValue">{{ modelValue }}</option>
+			<option v-if="all" v-bind:value="null" v-bind:selected="modelValue === null">{{ all }}</option>
 			<option v-for="(label, key) in items" v-bind:key="key" v-bind:value="key">
 				{{ label || key }}
 			</option>
 		</select>
 	`,
-	emits: ['input'],
+	emits: ['update:modelValue'],
 	props: {
 		'all': {type: String, default: ''},
 		'items': {type: Object, required: true},
 		'text': {type: String, default: ''},
-		'value': {required: true}
+		'modelValue': {required: true}
 	}
 };
