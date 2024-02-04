17a5a573c8ed960b13a078bf7eecca6b506cfb6d
Aimeos <aimeos@aimeos.org>
2024-02-04 13:57:08 +0100

    Migrated page limit component to VueJS 3.x

--- js/components/page-limit.js
+++ js/components/page-limit.js
@@ -9,27 +9,27 @@ Aimeos.components['page-limit'] = {
 		<div class="page-limit btn-group dropup" role="group">
 			<button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown"
 				v-bind:tabindex="tabindex" aria-haspopup="true" aria-expanded="false">
-				{{ value }} <span class="caret"></span>
+				{{ modelValue }} <span class="caret"></span>
 			</button>
 			<ul class="dropdown-menu">
 				<li class="dropdown-item">
-					<a class="dropdown-link" v-on:click.prevent="$emit('input', 25)" href="#" v-bind:tabindex="tabindex">25</a>
+					<a class="dropdown-link" v-on:click.prevent="$emit('update:modelValue', 25)" href="#" v-bind:tabindex="tabindex">25</a>
 				</li>
 				<li class="dropdown-item">
-					<a class="dropdown-link" v-on:click.prevent="$emit('input', 50)" href="#" v-bind:tabindex="tabindex">50</a>
+					<a class="dropdown-link" v-on:click.prevent="$emit('update:modelValue', 50)" href="#" v-bind:tabindex="tabindex">50</a>
 				</li>
 				<li class="dropdown-item">
-					<a class="dropdown-link" v-on:click.prevent="$emit('input', 100)" href="#" v-bind:tabindex="tabindex">100</a>
+					<a class="dropdown-link" v-on:click.prevent="$emit('update:modelValue', 100)" href="#" v-bind:tabindex="tabindex">100</a>
 				</li>
 				<li class="dropdown-item">
-					<a class="dropdown-link" v-on:click.prevent="$emit('input', 250)" href="#" v-bind:tabindex="tabindex">250</a>
+					<a class="dropdown-link" v-on:click.prevent="$emit('update:modelValue', 250)" href="#" v-bind:tabindex="tabindex">250</a>
 				</li>
 			</ul>
 		</div>
 	`,
-	emits: ['input'],
+	emits: ['update:modelValue'],
 	props: {
-		'value': {type: Number, required: true},
-		'tabindex': {type: String, default: '1'}
+		modelValue: {type: Number, required: true},
+		tabindex: {type: String, default: '1'}
 	}
 };
