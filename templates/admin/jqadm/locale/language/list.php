4d7f79ebc2563cecb9aae55abcf79f6fe48d6cb0
Aimeos <aimeos@aimeos.org>
2024-02-15 13:46:43 +0100

    Fixed showing batch edit in list views

--- templates/admin/jqadm/locale/language/list.php
+++ templates/admin/jqadm/locale/language/list.php
@@ -147,7 +147,7 @@ $columnList = [
 				</thead>
 				<tbody>
 
-					<tr class="batch" style="display: none" v-show="batch">
+					<tr class="batch" v-bind:class="{show: batch}" v-show="batch">
 						<td colspan="<?= count( $fields ) + 2 ?>">
 							<div class="batch-header">
 								<div class="intro">
