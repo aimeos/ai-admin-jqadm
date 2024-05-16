43db9075b794842057354d0bb169e6490364490f
Aimeos <aimeos@aimeos.org>
2024-05-16 11:10:16 +0200

    Don't use media file path for security reasons

--- src/Admin/JQAdm/Service/Media/Standard.php
+++ src/Admin/JQAdm/Service/Media/Standard.php
@@ -327,6 +327,9 @@ class Standard
 
 		foreach( $data as $idx => $entry )
 		{
+			// disallow overwriting for security reasons
+			unset( $entry['media.url'], $entry['media.preview'], $entry['media.previews'] );
+
 			$id = $this->val( $entry, 'media.id', '' );
 			$type = $this->val( $entry, 'service.lists.type', 'default' );
 
