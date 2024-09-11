9be63cb4ca6a27115ead7746ee661192a3ba67c1
Aimeos <aimeos@aimeos.org>
2024-09-11 12:55:51 +0200

    Added password object to context for tests

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -93,6 +93,10 @@ class TestHelper
 		$ctx->setI18n( array( 'de' => $i18n ) );
 
 
+		$passwd = new \Aimeos\Base\Password\Standard();
+		$ctx->setPassword( $passwd );
+
+
 		$session = new \Aimeos\Base\Session\None();
 		$ctx->setSession( $session );
 
