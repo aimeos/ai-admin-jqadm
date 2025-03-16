476c435a8b62cfc96e7a34d4dab04b4e99b114e7
Aimeos <aimeos@aimeos.org>
2025-03-16 12:17:40 +0100

    Added he-tree JS component

--- js/libs.js
+++ js/libs.js
@@ -10,6 +10,9 @@ import CKEditor from '@ckeditor/ckeditor5-vue'
 import '@vueform/multiselect/themes/default.css'
 import Multiselect from '@vueform/multiselect'
 
+import { BaseTree, Draggable as DragTree } from '@he-tree/vue'
+import '@he-tree/vue/style/default.css'
+
 import 'flatpickr/dist/flatpickr.css'
 import flatpickr from 'vue-flatpickr-component'
 import l10n from 'flatpickr/dist/l10n/index.js'
@@ -35,6 +38,8 @@ globalThis.serialize = param
 globalThis.sprintf = require('sprintf-js').sprintf
 globalThis.createApp = createApp
 globalThis.Draggable = draggable
+globalThis.BaseTree = BaseTree
+globalThis.DragTree = DragTree
 globalThis.Multiselect = Multiselect
 globalThis.Flatpickr = flatpickr
 globalThis.FlatpickrL10n = l10n
