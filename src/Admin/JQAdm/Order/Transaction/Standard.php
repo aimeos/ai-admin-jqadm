b83d106c71f39037836447046058b05e7ae00ef3
Oliver Kleinecke <okleinecke@web.de>
2026-05-29 08:34:33 +0200

    Fix strict comparison causing refund to trigger on empty transaction form (#314)

--- src/Admin/JQAdm/Order/Transaction/Standard.php
+++ src/Admin/JQAdm/Order/Transaction/Standard.php
@@ -226,7 +226,7 @@ class Standard
 
 		foreach( $data as $serviceId => $entry )
 		{
-			if( array_sum( array_map( 'floatval', (array) $entry ) ) === 0 || ( $service = $services->get( $serviceId ) ) === null ) {
+			if( array_sum( array_map( 'floatval', (array) $entry ) ) == 0 || ( $service = $services->get( $serviceId ) ) === null ) {
 				continue;
 			}
 
